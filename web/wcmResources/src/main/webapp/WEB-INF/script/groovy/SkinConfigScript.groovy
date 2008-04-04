8289b37a3f9594efbcdc37c0a8f88a16e56de652
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-04-04 03:13:33 +0000

    add wcmResource module

---
+++ web/wcmResources/src/main/webapp/WEB-INF/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,20 @@
+SkinService.addSkin(
+    "PortalSkin",
+    "Default",
+    "/web-presentation/skin/webui/component/UISimplePresentation/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "PortalSkin",
+    "Vista",
+    "/web-presentation/skin/webui/component/UISimplePresentation/VistaStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "PortalSkin",
+    "Mac",
+    "/web-presentation/skin/webui/component/UISimplePresentation/MacStylesheet.css",
+    ServletContext
+) ;
