ff15c1c336fff1e5faed4736dd6e3c3cf45d7bb7
Do Ngoc Anh <anh.do@exoplatform.com>
2008-05-29 10:21:43 +0000

    Create page template with SCP portlet   WCM-73

---
+++ web/wcmportal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/page/PageConfigOptions.groovy
@@ -0,0 +1,38 @@
+import org.exoplatform.webui.core.model.SelectItemOption;
+import org.exoplatform.webui.core.model.SelectItemCategory;
+
+List categories = new ArrayList(); 
+
+SelectItemCategory normalPageConfigs = new SelectItemCategory("normalPageConfigs") ;
+categories.add(normalPageConfigs);
+normalPageConfigs.addSelectItemOption(new SelectItemOption("Empty Layout", "empty", "EmptyLayout"));
+normalPageConfigs.addSelectItemOption(new SelectItemOption("Desktop Layout", "desktop", "DesktopImage"));
+
+SelectItemCategory columnPageConfigs = new SelectItemCategory("columnPageConfigs") ;
+categories.add(columnPageConfigs);  
+columnPageConfigs.addSelectItemOption(new SelectItemOption("Two Columns Layout", "two-columns", "TwoColumnsLayout"));
+columnPageConfigs.addSelectItemOption(new SelectItemOption("Three Columns Layout", "three-columns", "ThreeColumnsLayout"));
+
+SelectItemCategory rowPageConfigs = new SelectItemCategory("rowPageConfigs") ;
+categories.add(rowPageConfigs); 
+rowPageConfigs.addSelectItemOption(new SelectItemOption("Two Rows Layout", "two-rows", "TwoRowsLayout"));
+rowPageConfigs.addSelectItemOption(new SelectItemOption("Three Rows Layout", "three-rows", "ThreeRowsLayout"));
+
+SelectItemCategory tabsPageConfigs = new SelectItemCategory("tabsPageConfigs") ;
+categories.add(tabsPageConfigs) ;
+tabsPageConfigs.addSelectItemOption(new SelectItemOption("Two Tabs", "two-tabs", "TwoTabsLayout")) ;
+tabsPageConfigs.addSelectItemOption(new SelectItemOption("Three Tabs", "three-tabs", "ThreeTabsLayout")) ;
+
+SelectItemCategory mixPageConfigs = new SelectItemCategory("mixPageConfigs") ;
+categories.add(mixPageConfigs); 
+mixPageConfigs.addSelectItemOption(new SelectItemOption("Two Columns One Row Layout", "two-columns-one-row", "TwoColumnsOneRowLayout"));
+mixPageConfigs.addSelectItemOption(new SelectItemOption("One Row Two Columns Layout", "one-row-two-columns", "OneRowTwoColumnsLayout"));
+mixPageConfigs.addSelectItemOption(new SelectItemOption("Three Rows Two Columns Layout", "three-rows-two-columns", "ThreeRowsTwoColumnsLayout"));
+
+SelectItemCategory appPageConfigs = new SelectItemCategory("Application Page Configs") ;
+categories.add(appPageConfigs) ;
+appPageConfigs.addSelectItemOption(new SelectItemOption("Presentation", "simple-content-presentation-page", "Presentation")) ;
+appPageConfigs.addSelectItemOption(new SelectItemOption("Search", "simple-searches-page", "Search")) ;
+appPageConfigs.addSelectItemOption(new SelectItemOption("Sitemap", "sitemap-page", "Sitemap")) ;
+
+return categories;
