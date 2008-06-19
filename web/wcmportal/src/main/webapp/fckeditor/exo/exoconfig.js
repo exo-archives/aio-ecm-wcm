b43ab8b8008197103190e2f172da0dc91d60d513
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-06-19 03:43:32 +0000

    clear code in file exoconfig.js

--- web/wcmportal/src/main/webapp/fckeditor/exo/exoconfig.js
+++ web/wcmportal/src/main/webapp/fckeditor/exo/exoconfig.js
@@ -1,6 +1,8 @@
+//config eXo plugin bar
 FCKConfig.ToolbarSets["eXoBar"] = [
 	['Insert Image', 'Insert Portal Link', 'Insert DMS Document', 'Explorer']
 ];
+
 // set eXo plugin path;
 FCKConfig.eXoPath = FCKConfig.BasePath.substr(0, FCKConfig.BasePath.length - 7) + "exo/" ;
 FCKConfig.Plugins.Add( 'urani', null, FCKConfig.eXoPath + "plugins/") ;
@@ -9,73 +11,76 @@ FCKConfig.Plugins.Add( 'insertImage', null, FCKConfig.eXoPath + "plugins/") ;
 FCKConfig.Plugins.Add( 'insertDocument', null, FCKConfig.eXoPath + "plugins/") ;
 FCKConfig.Plugins.Add( 'insertPortalLink', null, FCKConfig.eXoPath + "plugins/") ;
 
-var eXoPlugin = {};
-var parentLocation = window.parent.location;
-eXoPlugin.hostName = parentLocation.href.substring(0, parentLocation.href.indexOf(parentLocation.pathname));
-
-with (window.parent.eXo.env.portal) {
-	eXoPlugin.portalName = portalName;
-	eXoPlugin.context = context;
-	eXoPlugin.accessMode = accessMode;
-}
-eXoPlugin.originPortalName = eXoPlugin.portalName;
-
-eXoPlugin.eXoFileManager = {
-	Connector: "/portal/rest/fckconnector/jcr/",
-	ResourceType : "File"
-};
-
-eXoPlugin.ExoImageBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=Image&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
-eXoPlugin.ExoFileBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=File&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
-eXoPlugin.ExoPortalLinkBrowserURL = FCKConfig.eXoPath + 'filemanager/portal/default/browser.html?Type=PortalLink&Connector=/portal/rest/fckconnectorext/pageURI' ;
-
-
-eXoPlugin.switchToolBar = function(R) {
-	var Setting = {
-		oldBar: R.oldBar || "" ,
-		newBar: R.newBar || "",
-		useBar: R.useBar || []
-	};
-	with (Setting) {
-		if (oldBar && newBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[oldBar]) {
-			FCKConfig.ToolbarSets[oldBar] = FCKConfig.ToolbarSets[newBar];
+//eXoPlugin config
+window.eXoPlugin = {
+	init: function() {
+		with (window.parent.eXo.env.portal) {
+			this.originPortalName = this.portalName = portalName;
+			this.context = context;
+			this.accessMode = accessMode;
 		}
-	}
-};
-
-//eXoPlugin.switchToolBar({oldBar: "Basic", newBar: "eXoBar"});
-eXoPlugin.addBar = function(R) {
-	var Setting = {
-		newBar: R.newBar || "",
-		targetBar: R.targetBar || ""
-	}
-
-	with (Setting) {
-		if (newBar == targetBar) return;
-		if (newBar && targetBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[targetBar]) {
-			FCKConfig.ToolbarSets[targetBar].push("/");
-			for (var o = 0; o < FCKConfig.ToolbarSets[newBar].length; ++o) {
-				FCKConfig.ToolbarSets[targetBar].push(FCKConfig.ToolbarSets[newBar][o]);
+		
+		var parentLocation = window.parent.location;
+		this.hostName = parentLocation.href.substring(0, parentLocation.href.indexOf(parentLocation.pathname));
+		
+		this.eXoFileManager = {
+			Connector: "/portal/rest/fckconnector/jcr/",
+			ResourceType : "File"
+		};
+		
+		this.ExoImageBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=Image&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
+		this.ExoFileBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=File&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
+		this.ExoPortalLinkBrowserURL = FCKConfig.eXoPath + 'filemanager/portal/default/browser.html?Type=PortalLink&Connector=/portal/rest/fckconnectorext/pageURI' ;
+		//detect user language
+		this.userLanguage = FCK.Language.GetActiveLanguage();
+	},
+	switchToolBar: function(r) {
+		var Setting = {
+			oldBar: r.oldBar || "" ,
+			newBar: r.newBar || "",
+			useBar: r.useBar || []
+		};
+		with (Setting) {
+			if (oldBar && newBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[oldBar]) {
+				FCKConfig.ToolbarSets[oldBar] = FCKConfig.ToolbarSets[newBar];
 			}
 		}
+		//demo =>  eXoPlugin.switchToolBar({oldBar: "Basic", newBar: "eXoBar"});
+	},
+	addBar: function(r) {
+		var Setting = {
+			newBar: r.newBar || "",
+			targetBar: r.targetBar || ""
+		}
+
+		with (Setting) {
+			if (newBar == targetBar) return;
+			if (newBar && targetBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[targetBar]) {
+				FCKConfig.ToolbarSets[targetBar].push("/");
+				for (var o = 0; o < FCKConfig.ToolbarSets[newBar].length; ++o) {
+					FCKConfig.ToolbarSets[targetBar].push(FCKConfig.ToolbarSets[newBar][o]);
+				}
+			}
+		}
+		//demo => eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Basic" });
+	},
+	getContent: function() {
+		var content = new String();
+		if (document.selection) {
+			var range = FCK.EditorWindow.document.selection.createRange();
+			content = range.text;
+		} else  {
+			var range = FCK.EditorWindow.getSelection();
+			content = range.getRangeAt(0);
+		}
+		if (content) content = content.toString().replace(/^\s+|\s+$/g, "");
+		if (content != "") return content;
+		else return null;
 	}
 };
+FCK["eXoPlugin"] = eXoPlugin;
 
-eXoPlugin.getContent = function() {
-	var content = new String();
-	if (document.selection) {
-		var range = FCK.EditorWindow.document.selection.createRange();
-		content = range.text;
-	} else  {
-		var range = FCK.EditorWindow.getSelection();
-		content = range.getRangeAt(0);
-	}
-	if (content) content = content.toString().replace(/^\s+|\s+$/g, "");
-	if (content != "") return content;
-	else return null;
-};
 
+eXoPlugin.init();
 eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Basic" });
 eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Default" });
-
-FCK["eXoPlugin"] = eXoPlugin;
