41a5f5fabe0fcc0cbf36d999666489e4f161696e
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2008-06-27 03:24:06 +0000

    test

--- webui/wcm/src/main/java/org/exoplatform/dms/application/JCRResourceResolver.java
+++ webui/wcm/src/main/java/org/exoplatform/dms/application/JCRResourceResolver.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
-package org.exoplatform.dms.application ; 
+package org.exoplatform.dms.application ;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
