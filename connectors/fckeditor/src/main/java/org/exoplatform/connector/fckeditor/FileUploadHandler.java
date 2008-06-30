6d1741516b5988d8efbbd3611131da54c706a6f9
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-06-30 09:30:37 +0000

    WCM-87 fix buid wcm bug

--- connectors/fckeditor/src/main/java/org/exoplatform/connector/fckeditor/FileUploadHandler.java
+++ connectors/fckeditor/src/main/java/org/exoplatform/connector/fckeditor/FileUploadHandler.java
@@ -43,7 +43,7 @@ public class FileUploadHandler {
   
   public void upload(String uploadId, String fileName, 
       InputStream data, double contentLength, String mimetype) throws Exception {
-    uploadService.createUploadResource(uploadId,fileName,data,contentLength,mimetype);    
+    //uploadService.createUploadResource(uploadId,fileName,data,contentLength,mimetype);    
   }
   
   public void refreshProgress(String uploadId) throws Exception { }
@@ -53,6 +53,6 @@ public class FileUploadHandler {
   public void delete(String uploadId) throws Exception {}
   
   public void saveAsNTFile(String uploadId,Node parent) { 
-    UploadResource resource = uploadService.getUploadResource(uploadId) ;    
+    //UploadResource resource = uploadService.getUploadResource(uploadId) ;    
   }
 }
