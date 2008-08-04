e8aafa4b00dceb9937933af271242530946a671a
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-08-04 10:05:00 +0000

    import css into editarea

--- web/wcmportal/src/main/webapp/fckeditor/exo/exoconfig.js
+++ web/wcmportal/src/main/webapp/fckeditor/exo/exoconfig.js
@@ -1,78 +1,104 @@
-	
-var eXoPlugin = {};
-var parentLocation = window.parent.location;
-eXoPlugin.hostName = parentLocation.href.substring(0, parentLocation.href.indexOf(parentLocation.pathname));
-eXoPlugin.portalName = window.parent.eXo.env.portal.portalName;
-eXoPlugin.originPortalName = eXoPlugin.portalName;
-
-eXoPlugin.availablePortals = ['classic' , 'webos'];
-
-with (window.parent.eXo.env.portal) {
-	eXoPlugin.context = context;
-	eXoPlugin.accessMode = accessMode;
-}
-
+//config eXo plugin bar
 FCKConfig.ToolbarSets["eXoBar"] = [
-	['Insert Image', 'Insert Portal Link', 'Insert DMS Document']
+	['Insert Image', 'Insert Portal Link', 'Insert DMS Document', 'Explorer']
 ];
 
-// set eXo  plugin path;
+// set eXo plugin path;
 FCKConfig.eXoPath = FCKConfig.BasePath.substr(0, FCKConfig.BasePath.length - 7) + "exo/" ;
 FCKConfig.Plugins.Add( 'urani', null, FCKConfig.eXoPath + "plugins/") ;
+FCKConfig.Plugins.Add( 'explorer', null, FCKConfig.eXoPath + "plugins/") ;
 FCKConfig.Plugins.Add( 'insertImage', null, FCKConfig.eXoPath + "plugins/") ;
 FCKConfig.Plugins.Add( 'insertDocument', null, FCKConfig.eXoPath + "plugins/") ;
 FCKConfig.Plugins.Add( 'insertPortalLink', null, FCKConfig.eXoPath + "plugins/") ;
 
-eXoPlugin.ExoImageBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=Image&Connector=/portal/connector' ;
-eXoPlugin.ExoFileBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=File&Connector=/portal/connector' ;
-eXoPlugin.ExoPortalLinkBrowserURL = FCKConfig.eXoPath + 'filemanager/portal/default/browser.html?Type=PortalLink&Connector=/portal/command' ;
+//config style
+FCKConfig.EditorAreaCSS = ['/eXoResources/skin/Stylesheet.css', '/wcmResources/skin/Stylesheet.css', '/portal/css/WebContent/Live/Stylesheet.css'] ;
+FCKConfig.EditorAreaStyles = 'body{	background: none;	margin: 0px;}' ;
 
-eXoPlugin.switchToolBar = function(R) {
-	var Setting = {
-		oldBar: R.oldBar || "" ,
-		newBar: R.newBar || "",
-		useBar: R.useBar || []
-	};
-	with (Setting) {
-		if (oldBar && newBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[oldBar]) {
-			FCKConfig.ToolbarSets[oldBar] = FCKConfig.ToolbarSets[newBar];
+//eXoPlugin config
+window.eXoPlugin = {
+	init: function() {
+		with (window.parent.eXo.env.portal) {
+   this.originPortalName = this.portalName = portalName;
+			this.context = context;
+			this.accessMode = accessMode;
 		}
-	}
-};
-
-//eXoPlugin.switchToolBar({oldBar: "Basic", newBar: "eXoBar"});
-eXoPlugin.addBar = function(R) {
-	var Setting = {
-		newBar: R.newBar || "",
-		targetBar: R.targetBar || ""
-	}
-
-	with (Setting) {
-		if (newBar == targetBar) return;
-		if (newBar && targetBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[targetBar]) {
-			FCKConfig.ToolbarSets[targetBar].push("/");
-			for (var o = 0; o < FCKConfig.ToolbarSets[newBar].length; ++o) {
-				FCKConfig.ToolbarSets[targetBar].push(FCKConfig.ToolbarSets[newBar][o]);
+		
+		var parentLocation = window.parent.location;
+		this.hostName = parentLocation.href.substring(0, parentLocation.href.indexOf(parentLocation.pathname));
+		
+		this.eXoFileManager = {
+			Connector: "/portal/rest/fckconnector/jcr/",
+			ResourceType : "File"
+		};
+		
+		this.ExoImageBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=Image&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
+		this.ExoFileBrowserURL = FCKConfig.eXoPath + 'filemanager/browser/default/browser.html?Type=File&Connector=/portal/rest/fckconnector/jcr/getFiles' ;
+		this.ExoPortalLinkBrowserURL = FCKConfig.eXoPath + 'filemanager/portal/default/browser.html?Type=PortalLink&Connector=/portal/rest/portalLinks/pageURI' ;
+		FCKConfig.LinkBrowserURL = FCKConfig.eXoPath + 'explorer/explorer.html?Type=Link&Connector=/portal/rest/wcmLink/';
+		//detect user language
+		this.userLanguage = FCK.Language.GetActiveLanguage() || "en";
+	},
+	switchToolBar: function(r) {
+		var Setting = {
+			oldBar: r.oldBar || "" ,
+			newBar: r.newBar || "",
+			useBar: r.useBar || []
+		};
+		with (Setting) {
+			if (oldBar && newBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[oldBar]) {
+				FCKConfig.ToolbarSets[oldBar] = FCKConfig.ToolbarSets[newBar];
 			}
 		}
+		//demo =>  eXoPlugin.switchToolBar({oldBar: "Basic", newBar: "eXoBar"});
+	},
+	addBar: function(r) {
+		var Setting = {
+			newBar: r.newBar || "",
+			targetBar: r.targetBar || ""
+		}
+
+		with (Setting) {
+			if (newBar == targetBar) return;
+			if (newBar && targetBar && FCKConfig.ToolbarSets[newBar] && FCKConfig.ToolbarSets[targetBar]) {
+				FCKConfig.ToolbarSets[targetBar].push("/");
+				for (var o = 0; o < FCKConfig.ToolbarSets[newBar].length; ++o) {
+					FCKConfig.ToolbarSets[targetBar].push(FCKConfig.ToolbarSets[newBar][o]);
+				}
+			}
+		}
+		//demo => eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Basic" });
+	},
+	getContent: function() {
+		var content = "";
+		if (document.selection) {
+			var range = FCK.EditorWindow.document.selection.createRange();
+			content = range.text;
+		} else  {
+			var range = FCK.EditorWindow.getSelection();
+			content = range.getRangeAt(0);
+		}
+		if (content) content = content.toString().replace(/^\s+|\s+$/g, "");
+		if (content != "") return content;
+		else return null;
+	},
+	loadScript: function() {
+		if (arguments.length < 2) {
+			return;
+		} else {
+			var win = arguments[0];
+			var src = arguments[1];
+		}
+		if (!win || !win.document) return;
+		var eScript = win.document.createElement("script");
+		eScript.setAttribute("src", src);
+		var eHead = win.document.getElementsByTagName("head")[0];
+		eHead.appendChild(eScript);
 	}
 };
+FCK["eXoPlugin"] = eXoPlugin;
 
-eXoPlugin.getContent = function() {
-	var content = new String();
-	if (document.selection) {
-		var range = FCK.EditorWindow.document.selection.createRange();
-		content = range.text;
-	} else  {
-		var range = FCK.EditorWindow.getSelection();
-		content = range.getRangeAt(0);
-	}
-	if (content) content = content.toString().replace(/^\s+|\s+$/g, "");
-	if (content != "") return content;
-	else return null;
-};
 
+eXoPlugin.init();
 eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Basic" });
 eXoPlugin.addBar({newBar: "eXoBar", targetBar: "Default" });
-
-FCK["eXoPlugin"] = eXoPlugin;
