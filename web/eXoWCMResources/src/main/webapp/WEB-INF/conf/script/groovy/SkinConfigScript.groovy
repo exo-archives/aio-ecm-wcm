9837c7d01ca7311dfdbe9bcdc7612f9361fe1315
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-08-07 04:46:50 +0000

    update

--- web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,7 +1,3 @@
-SkinService.addPortalSkin(
-    "wcmResources",
-    "Default",
-    "/wcmResources/skin/Stylesheet.css",
-    ServletContext,
-    true
-);
+SkinService.addPortalSkin("eXoWCMSkin", "Default", "/eXoWCMResources/skin/DefaultSkin/Stylesheet.css", ServletContext,true);
+SkinService.addPortalSkin("eXoWCMSkin", "Mac", "/eXoWCMResources/skin/MacSkin/Stylesheet.css", ServletContext,true);
+SkinService.addPortalSkin("eXoWCMSkin", "Vista", "/eXoWCMResources/skin/VistaSkin/Stylesheet.css", ServletContext,true);
