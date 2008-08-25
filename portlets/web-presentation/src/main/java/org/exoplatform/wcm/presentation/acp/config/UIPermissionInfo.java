04f368114c182aa7354ed7160ebd0308cf66ced5
Do Ngoc Anh <anh.do@exoplatform.com>
2008-08-25 09:50:53 +0000

    WCM-129

--- portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIPermissionInfo.java
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIPermissionInfo.java
@@ -58,7 +58,8 @@ import org.exoplatform.webui.event.EventListener;
 @ComponentConfig(
     lifecycle = UIContainerLifecycle.class, 
     events = {
-      @EventConfig(listeners = UIPermissionInfo.DeleteActionListener.class, confirm = "UIPermissionInfo.msg.confirm-delete-permission"),
+      @EventConfig(listeners = UIPermissionInfo.DeleteActionListener.class, 
+          confirm = "UIPermissionInfo.msg.confirm-delete-permission"),
       @EventConfig(listeners = UIPermissionInfo.EditActionListener.class) 
     }
 )
