a37513d32990bd20496bfb60565ef77861a23742
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-08-29 10:49:30 +0000

    read time insert CSS in to FCK Editor

--- web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
+++ web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
@@ -1,15 +1,21 @@
 
 window.wcm = function() {}
 
-wcm.insertCSSFromTextArea2FCK = function(Instance) {
-	var eContentCSS = document.getElementById("ContentCSS");
-	eContentCSS.style.border = "1px solid red";
-	eContentCSS.onblur = function() {
+wcm.insertCSSFromTextArea2FCK = function(Instance, ContentCSS) {
+	var eContentCSS = document.getElementById(ContentCSS);
+	var sContentCSSId = ContentCSS + "_Inline";
+	eContentCSS.onblur = updateStyle;
+	function updateStyle() {
 		var sValue = eContentCSS.value;
 		var iDoc = FCKeditorAPI.Instances[Instance].EditorWindow.document;
 		var eHead = iDoc.getElementsByTagName("head")[0];
-		var eStyle = iDoc.createElement("style");
+		var eStyle = iDoc.getElementById(sContentCSSId);
+		if (eStyle) {
+			eHead.removeChild(eStyle);
+		}
+		eStyle = iDoc.createElement("style");
 		eStyle.setAttribute("type", "text/css");
+		eStyle.setAttribute("id", sContentCSSId);
 		eStyle.innerHTML = sValue;
 		eHead.appendChild(eStyle);
 	}
