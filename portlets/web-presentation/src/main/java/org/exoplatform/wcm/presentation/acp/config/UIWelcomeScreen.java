2f18aba4b3eca56fe159ab1dcbeec9489b83195f
Dang Tan Dung <dung.dang@exoplatform.com>
2008-09-04 23:27:51 +0000

    WCM-170

--- portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIWelcomeScreen.java
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIWelcomeScreen.java
@@ -20,12 +20,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletPreferences;
+
+import org.exoplatform.portal.webui.util.SessionProviderFactory;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.wcm.presentation.acp.UIAdvancedPresentationPortlet;
 import org.exoplatform.wcm.presentation.acp.config.quickcreation.UIQuickCreationWizard;
+import org.exoplatform.wcm.presentation.acp.config.selector.UIDMSSelectorForm;
 import org.exoplatform.wcm.presentation.acp.config.selector.UIWebContentSelectorForm;
 import org.exoplatform.web.application.RequestContext;
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
-import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.core.model.SelectItemOption;
@@ -44,7 +55,8 @@ import org.exoplatform.webui.form.UIFormRadioBoxInput;
     lifecycle = UIFormLifecycle.class, 
     template = "app:/groovy/advancedPresentation/config/UIWelcomeScreen.gtmpl",
     events = {
-      @EventConfig(listeners = UIWelcomeScreen.StartProcessActionListener.class)
+      @EventConfig(listeners = UIWelcomeScreen.StartProcessActionListener.class),
+      @EventConfig(listeners = UIWelcomeScreen.BackActionListener.class)
     }
 )
 public class UIWelcomeScreen extends UIForm {
@@ -56,25 +68,46 @@ public class UIWelcomeScreen extends UIForm {
     List<SelectItemOption<String>> option = new ArrayList<SelectItemOption<String>>();
     RequestContext context = RequestContext.<RequestContext>getCurrentInstance();
     ResourceBundle res = context.getApplicationResourceBundle();
-    String labelQuickCreate = res.getString(getId() + ".label.QuickCreate");
-    String labelSelectExisted = res.getString(getId() + ".label.SelectExisted");
-    String labelEditContent = res.getString(getId() + ".label.EditContent");
-    String labelSelectOther = res.getString(getId() + ".label.SelectOther");
+    String labelQuickCreate = res.getString(getId() + ".label.QuickCreateWebContent");
+    String labelSelectExistedWebContent = res.getString(getId() + ".label.SelectExistedWebContent");
+    String labelSelectExistedDMS = res.getString(getId() + ".label.SelectExistedDMS");
+    String labelEditContent = res.getString(getId() + ".label.EditWebContent");
+    String labelSelectOther = res.getString(getId() + ".label.SelectOtherWebContent");
 
     if(isNewConfig) {
-      option.add(new SelectItemOption<String>(labelQuickCreate, "QuickCreate"));
-      option.add(new SelectItemOption<String>(labelSelectExisted, "SelectExisted"));
+      option.add(new SelectItemOption<String>(labelQuickCreate, "QuickCreateWebContent"));
+      option.add(new SelectItemOption<String>(labelSelectExistedWebContent, "SelectExistedWebContent"));
+      option.add(new SelectItemOption<String>(labelSelectExistedDMS,"SelectExistedDMS"));
       UIFormRadioBoxInput radioInput = new UIFormRadioBoxInput("radio", "radio", option);
       radioInput.setAlign(UIFormRadioBoxInput.VERTICAL_ALIGN);
-      radioInput.setValue("QuickCreate");
+      radioInput.setValue("QuickCreateWebContent");
       addUIFormInput(radioInput);
     }else {
-      option.add(new SelectItemOption<String>(labelEditContent, "EditCurrentContent"));
-      option.add(new SelectItemOption<String>(labelSelectOther, "SelectOtherContent"));
-      UIFormRadioBoxInput radioInput = new UIFormRadioBoxInput("radio", "radio", option);
-      radioInput.setAlign(UIFormRadioBoxInput.VERTICAL_ALIGN);
-      radioInput.setValue("EditCurrentContent");
-      addUIFormInput(radioInput);
+      PortletRequestContext pContext = (PortletRequestContext) WebuiRequestContext.getCurrentInstance();
+      PortletPreferences prefs = pContext.getRequest().getPreferences();
+      String repositoryName = prefs.getValue(UIAdvancedPresentationPortlet.REPOSITORY, null);
+      String workspaceName = prefs.getValue(UIAdvancedPresentationPortlet.WORKSPACE, null);
+      String UUID = prefs.getValue(UIAdvancedPresentationPortlet.UUID, null);
+      RepositoryService repositoryService = getApplicationComponent(RepositoryService.class);
+      ManageableRepository manageableRepository = repositoryService.getRepository(repositoryName);
+      Session session = SessionProviderFactory.createSystemProvider().getSession(workspaceName, manageableRepository);
+      Node node = session.getNodeByUUID(UUID);
+      if(node.getPrimaryNodeType().equals("exo:webContent")) {
+        option.add(new SelectItemOption<String>(labelEditContent, "EditCurrentWebContent"));
+        option.add(new SelectItemOption<String>(labelSelectOther, "SelectOtherWebContent"));
+        option.add(new SelectItemOption<String>(labelSelectExistedDMS,"SelectExistedDMS"));
+        UIFormRadioBoxInput radioInput = new UIFormRadioBoxInput("radio", "radio", option);
+        radioInput.setAlign(UIFormRadioBoxInput.VERTICAL_ALIGN);
+        radioInput.setValue("EditCurrentWebContent");
+        addUIFormInput(radioInput);
+      } else {
+        option.add(new SelectItemOption<String>(labelSelectOther, "SelectOtherWebContent"));
+        option.add(new SelectItemOption<String>(labelSelectExistedDMS,"SelectExistedDMS"));
+        UIFormRadioBoxInput radioInput = new UIFormRadioBoxInput("radio", "radio", option);
+        radioInput.setAlign(UIFormRadioBoxInput.VERTICAL_ALIGN);
+        radioInput.setValue("SelectOtherWebContent");
+        addUIFormInput(radioInput);
+      }
     }
 
     return this ;
@@ -91,15 +124,24 @@ public class UIWelcomeScreen extends UIForm {
       UIWelcomeScreen uiWelcomeScreen = event.getSource();
       String radioValue = uiWelcomeScreen.<UIFormRadioBoxInput>getUIInput("radio").getValue();
       UIPortletConfig uiPortletConfig = uiWelcomeScreen.getAncestorOfType(UIPortletConfig.class);
-      UIApplication uiApplication = uiWelcomeScreen.getAncestorOfType(UIApplication.class);
-      if(radioValue.equals("QuickCreate") || radioValue.equals("EditCurrentContent")) {
+      if(radioValue.equals("QuickCreateWebContent") || radioValue.equals("EditCurrentWebContent")) {
         uiWelcomeScreen.setRendered(false);
         UIQuickCreationWizard uiQuickCreationWizard = uiPortletConfig.addChild(UIQuickCreationWizard.class, null, null);
         uiQuickCreationWizard.init();
-      } else if(radioValue.equals("SelectOtherContent") || radioValue.equals("SelectExisted")) {
+      } else if(radioValue.equals("SelectOtherWebContent") || radioValue.equals("SelectExistedWebContent")) {
         uiWelcomeScreen.setRendered(false);
         uiPortletConfig.addChild(UIWebContentSelectorForm.class, null, null);
+      } else if(radioValue.equals("SelectExistedDMS")) {
+        uiWelcomeScreen.setRendered(false);
+        uiPortletConfig.addChild(UIDMSSelectorForm.class, null, null);
       }
+    }
+  }
+  
+  public static class BackActionListener extends EventListener<UIWelcomeScreen> {
+    public void execute(Event<UIWelcomeScreen> event) throws Exception {
+      PortletRequestContext context = (PortletRequestContext) event.getRequestContext();
+      context.setApplicationMode(PortletMode.VIEW);
     }
   }
 }
