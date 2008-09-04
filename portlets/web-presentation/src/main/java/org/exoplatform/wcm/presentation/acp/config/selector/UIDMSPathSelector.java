98d8d873579a8c74e68b8567cedd6519f802e6d9
Dang Tan Dung <dung.dang@exoplatform.com>
2008-09-04 23:25:50 +0000

    WCM-170

---
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/selector/UIDMSPathSelector.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.wcm.presentation.acp.config.selector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jcr.Node;
+
+import org.exoplatform.ecm.webui.tree.UIBaseNodeTreeSelector;
+import org.exoplatform.ecm.webui.tree.selectone.UISelectPathPanel;
+import org.exoplatform.portal.webui.util.SessionProviderFactory;
+import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.services.cms.templates.TemplateService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.wcm.portal.LivePortalManagerService;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.lifecycle.UIContainerLifecycle;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : DANG TAN DUNG
+ *          dzungdev@gmail.com
+ * Sep 3, 2008  
+ */
+@ComponentConfig(lifecycle = UIContainerLifecycle.class)
+public class UIDMSPathSelector extends UIBaseNodeTreeSelector {
+
+  public UIDMSPathSelector() throws Exception {
+    addChild(UIDMSTreeBuilder.class, null, UIDMSTreeBuilder.class.getSimpleName() + hashCode());
+    addChild(UISelectPathPanel.class, null, UISelectPathPanel.class.getSimpleName() + hashCode());
+  }
+
+  public void init() throws Exception {
+    LivePortalManagerService livePortalManagerService = getApplicationComponent(LivePortalManagerService.class);
+    String currentPortalName = Util.getUIPortal().getName();
+    SessionProvider sessionProvider = SessionProviderFactory.createSessionProvider();
+    Node currentPortal = livePortalManagerService.getLivePortal(currentPortalName, sessionProvider);
+    Node sharedPortal = livePortalManagerService.getLiveSharedPortal(sessionProvider);
+    String repositoryName = ((ManageableRepository)(currentPortal.getSession().getRepository())).getConfiguration().getName();
+    TemplateService templateService = getApplicationComponent(TemplateService.class);
+    List<String> acceptedNodeTypes = templateService.getDocumentTemplates(repositoryName);
+    UISelectPathPanel uiSelectPathPanel = getChild(UISelectPathPanel.class);
+    uiSelectPathPanel.setAcceptedNodeTypes(acceptedNodeTypes);
+    UIDMSTreeBuilder treeBuilder = getChild(UIDMSTreeBuilder.class);
+    treeBuilder.setCurrentPortal(currentPortal);
+    treeBuilder.setSharedPortal(sharedPortal);
+    treeBuilder.setRootTreeNode(currentPortal.getParent());
+    sessionProvider.close();
+  }
+
+  @Override
+  public void onChange(Node node, Object context) throws Exception {
+    UISelectPathPanel uiSelectPathPanel = getChild(UISelectPathPanel.class);
+    uiSelectPathPanel.setParentNode(node);
+  }
+}
---
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/selector/UIDMSSelectorForm.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.wcm.presentation.acp.config.selector;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletPreferences;
+
+import org.exoplatform.ecm.webui.selector.UISelectable;
+import org.exoplatform.portal.webui.util.SessionProviderFactory;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.wcm.core.NodeIdentifier;
+import org.exoplatform.services.wcm.core.NodeLocation;
+import org.exoplatform.services.wcm.core.WCMConfigurationService;
+import org.exoplatform.wcm.presentation.acp.UIAdvancedPresentationPortlet;
+import org.exoplatform.webui.application.portlet.PortletRequestContext;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.webui.core.UIContainer;
+import org.exoplatform.webui.core.UIPopupWindow;
+import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
+import org.exoplatform.webui.form.UIForm;
+import org.exoplatform.webui.form.UIFormStringInput;
+import org.exoplatform.webui.form.ext.UIFormInputSetWithAction;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : DANG TAN DUNG
+ *          dzungdev@gmail.com
+ * Sep 3, 2008  
+ */
+@ComponentConfig (
+    lifecycle = UIFormLifecycle.class,
+    template = "system:/groovy/webui/form/UIFormWithTitle.gtmpl",
+    events = {
+      @EventConfig(listeners = UIDMSSelectorForm.SaveActionListener.class),
+      @EventConfig(listeners = UIDMSSelectorForm.BackActionListener.class),
+      @EventConfig(listeners = UIDMSSelectorForm.BrowseActionListener.class)
+    }
+)
+public class UIDMSSelectorForm extends UIForm implements UISelectable{
+
+  final static String PATH = "path".intern();
+  final static String FIELD_PATH = "location".intern();
+  private String repository;
+  private String workspace;
+  private String livePortalsPath;
+  private String liveSharedPortalName;
+
+  public UIDMSSelectorForm() throws Exception {
+    RepositoryService repoService = getApplicationComponent(RepositoryService.class);
+    String repoName = repoService.getCurrentRepository().getConfiguration().getName();
+    WCMConfigurationService configurationService = getApplicationComponent(WCMConfigurationService.class);
+    NodeLocation nodeLocation = configurationService.getLivePortalsLocation(repoName);
+    repository = nodeLocation.getRepository();
+    workspace = nodeLocation.getWorkspace();
+    livePortalsPath = nodeLocation.getPath();
+    liveSharedPortalName = configurationService.getSharedPortalName(repository);
+    UIFormInputSetWithAction uiPathSelection = new UIFormInputSetWithAction(FIELD_PATH);
+    uiPathSelection.addUIFormInput(new UIFormStringInput(PATH, PATH, null).setEditable(false));
+    uiPathSelection.setActionInfo(PATH, new String[] {"Browse"});
+    addChild(uiPathSelection);
+    setActions(new String[] {"Save","Back"});
+  }
+
+  public String getLivePortalPath() { return livePortalsPath; }
+  public String getLiveSharedPortalName() { return liveSharedPortalName; }
+  public String getRepositoryName() { return repository; }
+  public String getWorkspace() { return workspace; }
+
+  public void doSelect(String selectField, Object value) throws Exception {
+    getUIStringInput(selectField).setValue((String)value);
+    showPopupComponent(null);
+  }
+
+  public void showPopupComponent(UIComponent uiComponent) throws Exception {
+    UIContainer uiParent = getParent();
+    if(uiComponent == null) {
+      uiParent.removeChild(UIPopupWindow.class);
+      return ;
+    }
+    UIPopupWindow uiPopup = uiParent.getChild(UIPopupWindow.class);
+    if( uiPopup == null)  uiPopup = uiParent.addChild(UIPopupWindow.class, null, null);
+    uiPopup.setUIComponent(uiComponent);
+    uiPopup.setWindowSize(610, 300);
+    uiPopup.setResizable(true);
+    uiPopup.setShow(true);
+  }
+
+  public static class SaveActionListener extends EventListener<UIDMSSelectorForm> {
+    public void execute(Event<UIDMSSelectorForm> event) throws Exception {
+      UIDMSSelectorForm uiDMSSelectorForm = event.getSource();
+      RepositoryService repositoryService = uiDMSSelectorForm.getApplicationComponent(RepositoryService.class);
+      ManageableRepository manageableRepository = repositoryService.getRepository(uiDMSSelectorForm.getRepositoryName());
+      Session session = SessionProviderFactory.createSystemProvider().getSession(uiDMSSelectorForm.getWorkspace(), manageableRepository);
+      Node node = (Node) session.getItem(uiDMSSelectorForm.getUIStringInput(UIDMSSelectorForm.PATH).getValue());
+      NodeIdentifier nodeIdentifier = NodeIdentifier.make(node);
+      PortletRequestContext pContext = (PortletRequestContext) event.getRequestContext();
+      PortletPreferences prefs = pContext.getRequest().getPreferences();
+      prefs.setValue(UIAdvancedPresentationPortlet.REPOSITORY, nodeIdentifier.getRepository());
+      prefs.setValue(UIAdvancedPresentationPortlet.WORKSPACE, nodeIdentifier.getWorkspace());
+      prefs.setValue(UIAdvancedPresentationPortlet.UUID, nodeIdentifier.getUUID());
+      prefs.store();
+      pContext.setApplicationMode(PortletMode.VIEW ) ;
+    }
+  }
+
+  public static class BackActionListener extends EventListener<UIDMSSelectorForm> {
+    public void execute(Event<UIDMSSelectorForm> event) {
+
+    }
+  }
+
+  public static class BrowseActionListener extends EventListener<UIDMSSelectorForm> {
+    public void execute(Event<UIDMSSelectorForm> event) throws Exception {
+      UIDMSSelectorForm uiDMSSelectorForm = event.getSource();
+      UIDMSPathSelector uiDMSPathSelector = uiDMSSelectorForm.createUIComponent(UIDMSPathSelector.class, null, null);
+      uiDMSPathSelector.setSourceComponent(uiDMSSelectorForm, new String[] {UIDMSSelectorForm.PATH});
+      uiDMSPathSelector.init();
+      uiDMSSelectorForm.showPopupComponent(uiDMSPathSelector);
+    }
+  }
+
+}
