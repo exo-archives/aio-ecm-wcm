0674ccc4dcc0a81694093b677fe936a7776c68c9
Dang Tan Dung <dung.dang@exoplatform.com>
2008-09-05 03:08:17 +0000

    WCM-170

--- portlets/web-presentation/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ portlets/web-presentation/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,2 +1,2 @@
 /* External libraries */
-JavascriptService.addJavascript("eXo.webpresentation.Utils", "javascript/eXo/webpresentation/Utils.js", ServletContext);
+JavascriptService.addJavascript("eXo.webpresentation.Utils", "/javascript/eXo/webpresentation/Utils.js", ServletContext);
