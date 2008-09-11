2ccf676a09e33cef8badbe52d84e751d4956ed0f
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-09-11 04:13:48 +0000

    move some js to wcmResource

--- portlets/web-presentation/src/main/webapp/javascript/eXo/webpresentation/Utils.js
+++ portlets/web-presentation/src/main/webapp/javascript/eXo/webpresentation/Utils.js
@@ -1,29 +1,3 @@
-eXo.wcm = {
-	
-};
-
-Utils = function(){
-	Utils.prototype.removeQuickeditingBlock = function(portletID, quickEditingBlockId) {
-		var presentation = document.getElementById(portletID);
-		var parentNode = presentation.parentNode;
-		var quickEditingBlock = document.getElementById(quickEditingBlockId);
-		if(quickEditingBlock != null) {
-			quickEditingBlock.parentNode.removeChild(quickEditingBlock);
-		}
-	};
-		
-	Utils.prototype.insertQuickeditingBlock = function(portletID, quickEditingBlockId) {
-		var presentation = document.getElementById(portletID);
-		var parentNode = presentation.parentNode;
-		var quickEditingBlock = document.getElementById(quickEditingBlockId);
-		if(quickEditingBlock == null) {
-			parentNode.insertBefore(newEditingBlock, presentation);
-		}
-	};
-}
-
-eXo.wcm = new Utils();
-	
 var isOpen = false;
 function showDivBlock(checkpoint, div1Class, div2Class) {
 	var root = eXo.core.DOMUtil.findAncestorsByClass(checkpoint, 'UIWizard')[0];
