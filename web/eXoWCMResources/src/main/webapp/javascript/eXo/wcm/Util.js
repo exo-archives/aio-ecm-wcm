6d94813e24bff82e2981510be07db416ab967006
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-09-11 04:14:35 +0000

    move some js to wcmResource

--- web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
+++ web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
@@ -1,5 +1,4 @@
 window.wcm = function() {}
-
 wcm.insertCSSFromTextArea2FCK = function(Instance, ContentCSS) {
 	var eContentCSS = document.getElementById(ContentCSS);
 	var sContentCSSId = ContentCSS + "_Inline";
@@ -36,3 +35,24 @@ wcm.insertCSSFromTextArea2FCK = function(Instance, ContentCSS) {
 		}
 	})();
 }
+
+Utils = function(){
+	Utils.prototype.removeQuickeditingBlock = function(portletID, quickEditingBlockId) {
+		var presentation = document.getElementById(portletID);
+		var parentNode = presentation.parentNode;
+		var quickEditingBlock = document.getElementById(quickEditingBlockId);
+		if(quickEditingBlock != null) {
+			quickEditingBlock.parentNode.removeChild(quickEditingBlock);
+		}
+	};
+		
+	Utils.prototype.insertQuickeditingBlock = function(portletID, quickEditingBlockId) {
+		var presentation = document.getElementById(portletID);
+		var parentNode = presentation.parentNode;
+		var quickEditingBlock = document.getElementById(quickEditingBlockId);
+		if(quickEditingBlock == null) {
+			parentNode.insertBefore(newEditingBlock, presentation);
+		}
+	};
+}
+eXo.wcm = new Utils();
