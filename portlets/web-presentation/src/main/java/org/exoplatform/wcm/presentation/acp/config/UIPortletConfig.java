026033bcb2f1c0b19f06ba61025e502b5b2e2802
Dang Tan Dung <dung.dang@exoplatform.com>
2008-09-15 10:20:35 +0000

    WCM-168

--- portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIPortletConfig.java
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIPortletConfig.java
@@ -16,13 +16,27 @@
  */
 package org.exoplatform.wcm.presentation.acp.config;
 
+import java.util.List;
+
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.Node;
 import javax.jcr.Session;
 import javax.portlet.PortletPreferences;
 
+import org.exoplatform.portal.config.DataStorage;
+import org.exoplatform.portal.config.UserACL;
+import org.exoplatform.portal.config.model.PortalConfig;
 import org.exoplatform.portal.webui.util.SessionProviderFactory;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.access.AccessControlList;
+import org.exoplatform.services.jcr.access.PermissionType;
+import org.exoplatform.services.jcr.core.ExtendedNode;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.wcm.portal.LivePortalManagerService;
+import org.exoplatform.wcm.presentation.acp.UIAdvancedPresentationPortlet;
+import org.exoplatform.wcm.presentation.acp.UINonEditable;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
@@ -43,13 +57,48 @@ public class UIPortletConfig extends UIContainer {
   private UIComponent uiBackComponent;
   private boolean isNewConfig;
 
-  public UIPortletConfig() throws Exception {      
+  public UIPortletConfig() throws Exception {
     isNewConfig = checkNewConfig();
-    UIWelcomeScreen uiWellcomeScreen = createUIComponent(UIWelcomeScreen.class, null, null).setCreateMode(isNewConfig);
-    addChild(uiWellcomeScreen);
-
-    uiBackComponent = uiWellcomeScreen ;
+    if(isQuickEditable()) {
+      UIWelcomeScreen uiWellcomeScreen = createUIComponent(UIWelcomeScreen.class, null, null).setCreateMode(isNewConfig);
+      addChild(uiWellcomeScreen);
+      uiBackComponent = uiWellcomeScreen ;
+    } else {
+      addChild(UINonEditable.class, null, null);
+    }
   }
+
+  private boolean isQuickEditable() throws Exception {
+    PortletRequestContext context = (PortletRequestContext) WebuiRequestContext.getCurrentInstance();
+    PortletPreferences prefs = context.getRequest().getPreferences();
+    String repositoryName = prefs.getValue(UIAdvancedPresentationPortlet.REPOSITORY, null);
+    if(repositoryName == null) return true;
+    String portalName = Util.getUIPortal().getName();
+    String userId = context.getRemoteUser();
+    String quickEdit = prefs.getValue("ShowQuickEdit", null);
+    boolean isQuickEdit = Boolean.parseBoolean(quickEdit);
+    DataStorage dataStorage = getApplicationComponent(DataStorage.class);
+    PortalConfig portalConfig = dataStorage.getPortalConfig(portalName);
+    UserACL userACL = getApplicationComponent(UserACL.class);
+    boolean displayQuickEdit = userACL.hasEditPermission(portalConfig, userId);
+    LivePortalManagerService livePortalManagerService = getApplicationComponent(LivePortalManagerService.class);
+    SessionProvider sessionProvider = SessionProviderFactory.createSessionProvider();
+    Node portalNode = livePortalManagerService.getLivePortal(portalName, sessionProvider);
+    String UUID = prefs.getValue(UIAdvancedPresentationPortlet.UUID, null);
+    try{
+      ExtendedNode exWebContentNode = (ExtendedNode) portalNode.getSession().getNodeByUUID(UUID);
+      AccessControlList acl = exWebContentNode.getACL();
+      List<String> permList = acl.getPermissions(userId);
+      if(permList.contains(PermissionType.ADD_NODE) && permList.contains(PermissionType.REMOVE) 
+          && permList.contains(PermissionType.SET_PROPERTY)) {
+        return (isQuickEdit && displayQuickEdit);
+      }
+    }catch(ItemNotFoundException e) {
+      return true;
+    }
+    return false;
+  }
+
   private boolean checkNewConfig(){
     PortletRequestContext portletRequestContext = WebuiRequestContext.getCurrentInstance() ;
     PortletPreferences prefs = portletRequestContext.getRequest().getPreferences() ;
