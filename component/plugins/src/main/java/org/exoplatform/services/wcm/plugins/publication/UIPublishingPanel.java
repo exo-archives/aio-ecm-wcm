273e4a8daa4b7e31a98f20fd4e65b7fb79959f52
Dang Tan Dung <dung.dang@exoplatform.com>
2008-09-30 10:58:24 +0000

    WCM-108 update

--- component/plugins/src/main/java/org/exoplatform/services/wcm/plugins/publication/UIPublishingPanel.java
+++ component/plugins/src/main/java/org/exoplatform/services/wcm/plugins/publication/UIPublishingPanel.java
@@ -16,8 +16,11 @@
  */
 package org.exoplatform.services.wcm.plugins.publication;
 
+import java.util.List;
+
 import javax.jcr.Node;
 
+import org.exoplatform.services.wcm.plugins.publication.UIPublicationTree.TreeNode;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
 import org.exoplatform.webui.form.UIForm;
@@ -30,23 +33,38 @@ import org.exoplatform.webui.form.UIForm;
  */
 @ComponentConfig (
     lifecycle = UIApplicationLifecycle.class,
-    template = "classpath:groovy/wcm/webui/publication/UIPublishingManager.gtmpl"
+    template = "classpath:groovy/wcm/webui/publication/UIPublishingPanel.gtmpl"
 )
 public class UIPublishingPanel extends UIForm {
-  
+
   private Node currentNode;
   
-  public UIPublishingPanel() throws Exception {
-    //Left panel
-    addChild(UIPortalNavigationExplorer.class,null,null);
-    //right panel
-    addChild(UIPublishedPages.class,null,null);
-  }
-  
-  public void initPanel(Node node) {
-    currentNode = node;
-  }
-  
-  public Node getNode(Node node) { return this.currentNode; }
+  public Node getNode() {return this.currentNode;}
   public void setNode(Node node) {this.currentNode = node; }  
+  
+  public UIPublishingPanel() throws Exception {
+    addChild(UIPortalNavigationExplorer.class,null,"UIPortalNavigationExplorer");
+    //addChild(UIPublishedPages.class,null,"UIPublishedPages");
+  }
+
+  public void initPanel(Node node,String portalName,List<String> runningPortals) throws Exception {
+    this.currentNode = node;
+    UIPortalNavigationExplorer poExplorer = getChild(UIPortalNavigationExplorer.class);
+    poExplorer.init(portalName,runningPortals);
+  }
+  
+  public String getCurrentPortal() {
+    UIPortalNavigationExplorer portalNavigationExplorer = getChild(UIPortalNavigationExplorer.class);
+    TreeNode selectedNode = portalNavigationExplorer.getSelectedNode();
+    if (selectedNode != null) return selectedNode.getPortalName();
+    return null;
+  }
+  
+  public String getCurrentTreeNode() {
+    UIPortalNavigationExplorer portalNavigationExplorer = getChild(UIPortalNavigationExplorer.class);
+    TreeNode selectedNode = portalNavigationExplorer.getSelectedNode();
+    if (selectedNode != null) return selectedNode.getName();
+    return null;
+  }
+  
 }
