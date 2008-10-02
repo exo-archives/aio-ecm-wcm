2bf45d5fbe3095532083da08ee8a3b5a5141c202
Dang Tan Dung <dung.dang@exoplatform.com>
2008-10-02 01:35:40 +0000

    WCM-251

--- portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIContentDialogForm.java
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/presentation/acp/config/UIContentDialogForm.java
@@ -27,6 +27,7 @@ import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.version.VersionException;
+import javax.portlet.PortletPreferences;
 
 import org.exoplatform.ecm.resolver.JCRResourceResolver;
 import org.exoplatform.ecm.webui.form.UIDialogForm;
@@ -44,9 +45,11 @@ import org.exoplatform.services.wcm.core.NodeLocation;
 import org.exoplatform.services.wcm.core.WebSchemaConfigService;
 import org.exoplatform.services.wcm.portal.LivePortalManagerService;
 import org.exoplatform.services.wcm.portal.PortalFolderSchemaHandler;
+import org.exoplatform.wcm.presentation.acp.UIAdvancedPresentationPortlet;
 import org.exoplatform.wcm.presentation.acp.config.quickcreation.UIQuickCreationWizard;
 import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIApplication;
@@ -76,6 +79,7 @@ public class UIContentDialogForm extends UIDialogForm {
   protected NodeLocation storedLocation;
   protected NodeIdentifier savedNodeIdentifier;
   protected Node webContent;
+  private boolean isCheckInOpened;
 
   public UIContentDialogForm() throws Exception {
     setActions(ACTIONS);
@@ -218,6 +222,9 @@ public class UIContentDialogForm extends UIDialogForm {
       }
       dialogForm.savedNodeIdentifier = NodeIdentifier.make(newNode);
       dialogForm.setWebContent(newNode);
+      if (dialogForm.isCheckInOpened()) {
+        newNode.checkin();
+      }
       UIQuickCreationWizard uiQuickWizard = dialogForm.getAncestorOfType(UIQuickCreationWizard.class);
       UIPermissionManager uiPermissionManager = uiQuickWizard.getChild(UIPermissionManager.class);
       ((UIPermissionInfo) uiPermissionManager.getChild(UIPermissionInfo.class)).updateGrid();
@@ -225,5 +232,13 @@ public class UIContentDialogForm extends UIDialogForm {
       uiSocialInfo.initUICategorizing();
       uiQuickWizard.viewStep(3);
     }
+  }
+
+  public boolean isCheckInOpened() {
+    return isCheckInOpened;
+  }
+
+  public void setCheckInOpened(boolean isCheckInOpened) {
+    this.isCheckInOpened = isCheckInOpened;
   }
 }
