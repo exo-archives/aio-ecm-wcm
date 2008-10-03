b26ceccd8eb709a0a457efc5afed92993f65d566
Do Ngoc Anh <anh.do@exoplatform.com>
2008-10-03 02:54:28 +0000

    change tooltip of insert portal link

--- web/wcmportal/src/main/webapp/fckeditor/exo/plugins/insertPortalLink/fckplugin.js
+++ web/wcmportal/src/main/webapp/fckeditor/exo/plugins/insertPortalLink/fckplugin.js
@@ -2,7 +2,7 @@ var initFun = function(O) {
 	// init function
 }
 
-FCKCommands.RegisterCommand( "Insert link", new FCKDialogCommand( "Insert Portal Link", "Insert Portal Link", FCKConfig.eXoPath + "plugins/insertPortalLink/insertPortalLink.html"	, 600, 400) ) ;
-var oInsertPortalLink = new FCKToolbarButton("Insert link") ;
+FCKCommands.RegisterCommand("Insert link to a site page", new FCKDialogCommand( "Insert Portal Link", "Insert Portal Link", FCKConfig.eXoPath + "plugins/insertPortalLink/insertPortalLink.html"	, 600, 400) ) ;
+var oInsertPortalLink = new FCKToolbarButton("Insert link to a site page") ;
 oInsertPortalLink.IconPath = FCKConfig.eXoPath + "plugins/insertPortalLink/insertPortalLink.gif" ;
-FCKToolbarItems.RegisterItem("Insert Portal Link", oInsertPortalLink) ;
+FCKToolbarItems.RegisterItem("Insert link to a site page", oInsertPortalLink) ;
