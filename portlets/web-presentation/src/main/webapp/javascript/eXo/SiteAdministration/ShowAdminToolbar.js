5343cf6a9e781334e6f00e01437af06bb6125cfc
Do Ngoc Anh <anh.do@exoplatform.com>
2008-10-13 11:18:13 +0000

    improve for site admin toolbar

--- portlets/web-presentation/src/main/webapp/javascript/eXo/SiteAdministration/ShowAdminToolbar.js
+++ portlets/web-presentation/src/main/webapp/javascript/eXo/SiteAdministration/ShowAdminToolbar.js
@@ -1,12 +1,4 @@
-function showMoreAction(expandItem, toolBarClazz, moreActionBlockId, hiddenActionClazz, evt) {
-	var root = eXo.core.DOMUtil.findAncestorByClass(expandItem, toolBarClazz);
-	var moreActionContainer = eXo.core.DOMUtil.findDescendantById(root, moreActionBlockId);
-	if (moreActionContainer.style.display == "block") {
-		moreActionContainer.style.display = "none";
-	} else {
-		moreActionContainer.style.display = "block";
-	}
-	evt = window.event || evt ;
-	evt.cancelBubble = true ;
-	eXo.core.DOMUtil.hideElementList.push(moreActionContainer);	
+function showWorkspaceArea() {
+	 eXo.portal.UIControlWorkspace.showControlWorkspace = false;
+	 eXo.portal.UIControlWorkspace.showWorkspace();
 }
