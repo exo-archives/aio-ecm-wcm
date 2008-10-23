262e29415dfb940693b6933736f9ccf9d8de057d
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-10-23 09:15:54 +0000

    WCM-329 add new site-atifacts-initialzer service

--- web/wcmportal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
+++ web/wcmportal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
@@ -3,18 +3,11 @@ import java.util.ArrayList;
 import org.exoplatform.portal.webui.portal.PortalTemplateConfigOption ;
 import org.exoplatform.webui.core.model.SelectItemCategory;
 
-List options = new ArrayList();
-
-  SelectItemCategory classic = new SelectItemCategory("ClassicSite");
-  classic.addSelectItemOption(
-      new PortalTemplateConfigOption("", "classic", "Classic Site", "ClassicSite").addGroup("/platform/administrators")
-  );
-  
+List options = new ArrayList();  
   SelectItemCategory acme = new SelectItemCategory("ACMESite");
   acme.addSelectItemOption(
       new PortalTemplateConfigOption("ACME Site", "acme", "ACME Site", "ACMESite").addGroup("/platform/administrators")
-  );
-  options.add(classic);
+  );  
   options.add(acme);
   
 return options ;
