88208a973550b448e0d94804bd627b742ae4e285
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-10-23 09:29:32 +0000

    WCM-328 add some new method

--- portlets/web-presentation/src/main/java/org/exoplatform/wcm/webui/clv/UIContentListPresentation.java
+++ portlets/web-presentation/src/main/java/org/exoplatform/wcm/webui/clv/UIContentListPresentation.java
@@ -16,16 +16,22 @@
  */
 package org.exoplatform.wcm.webui.clv;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.jcr.Node;
 import javax.portlet.PortletPreferences;
 
+import org.exoplatform.commons.utils.ISO8601;
 import org.exoplatform.commons.utils.PageList;
 import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.portal.webui.container.UIContainer;
 import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.resolver.ResourceResolver;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.UserProfile;
+import org.exoplatform.services.organization.UserProfileHandler;
 import org.exoplatform.services.wcm.core.WCMConfigurationService;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.application.portlet.PortletRequestContext;
@@ -43,34 +49,38 @@ import org.exoplatform.webui.event.EventListener;
  */
 
 @ComponentConfigs( {
-    @ComponentConfig(lifecycle = Lifecycle.class, events = @EventConfig(listeners = UIContentListPresentation.RefreshActionListener.class)),
-    @ComponentConfig(type = UIPageIterator.class, template = "system:/groovy/webui/core/UIPageIterator.gtmpl", events = @EventConfig(listeners = UIPageIterator.ShowPageActionListener.class)) })
-public class UIContentListPresentation extends UIContainer {
+  @ComponentConfig(lifecycle = Lifecycle.class, events = @EventConfig(listeners = UIContentListPresentation.RefreshActionListener.class)),
+  @ComponentConfig(type = UIPageIterator.class, template = "system:/groovy/webui/core/UIPageIterator.gtmpl", events = @EventConfig(listeners = UIPageIterator.ShowPageActionListener.class)) })
+  public class UIContentListPresentation extends UIContainer {  
   private String           templatePath;
-
   private ResourceResolver resourceResolver;
-
   private UIPageIterator   uiPageIterator;
+  private String contentColumn;  
+  private SimpleDateFormat dateFormatter = new SimpleDateFormat(ISO8601.SIMPLE_DATETIME_FORMAT);
 
-  public UIContentListPresentation() {
-  }
+  public UIContentListPresentation() { }
 
-  public void init(String templatePath, ResourceResolver resourceResolver, PageList dataPageList)
-      throws Exception {
+  public void init(String templatePath, ResourceResolver resourceResolver, PageList dataPageList) throws Exception {
     this.templatePath = templatePath;
     this.resourceResolver = resourceResolver;
     uiPageIterator = addChild(UIPageIterator.class, null, null);
-    uiPageIterator.setPageList(dataPageList);
+    uiPageIterator.setPageList(dataPageList);    
   }
 
   public String getTemplate() {
     return templatePath;
   }
-  
+
+  public SimpleDateFormat getDatetimeFommatter() { return dateFormatter; }
+  public void setDateTimeFormat(String format) { dateFormatter.applyPattern(format); }
+
+  public String getContentColumn() { return this.contentColumn; }
+  public void setContentColumn(String column) { this.contentColumn = column;}
+
   public UIPageIterator getUIPageIterator() { return uiPageIterator; }
   public ResourceResolver getTemplateResourceResolver(WebuiRequestContext context, String template) {
     return resourceResolver;
-  }
+  }   
 
   public List getCurrentPageData() throws Exception {
     return uiPageIterator.getCurrentPageData();
@@ -78,7 +88,7 @@ public class UIContentListPresentation extends UIContainer {
 
   public String getTitle(Node node) throws Exception {
     return node.hasProperty("exo:title") ? node.getProperty("exo:title").getValue().getString()
-        : null;
+        : node.getName();
   }
 
   public String getSummary(Node node) throws Exception {
@@ -98,27 +108,39 @@ public class UIContentListPresentation extends UIContainer {
     String workspace = portletPreferences.getValue(UIContentListViewerPortlet.WORKSPACE,
         UIContentListViewerPortlet.WORKSPACE);
     String baseURI = portletRequestContext.getRequest().getScheme() + "://"
-        + portletRequestContext.getRequest().getServerName() + ":"
-        + String.format("%s", portletRequestContext.getRequest().getServerPort());
+    + portletRequestContext.getRequest().getServerName() + ":"
+    + String.format("%s", portletRequestContext.getRequest().getServerPort());
     String parameterizedPageURI = wcmConfigurationService.getParameterizedPageURI();
     link = baseURI + portalURI + parameterizedPageURI.substring(1, parameterizedPageURI.length())
-        + "/" + repository + "/" + workspace + node.getPath();
+    + "/" + repository + "/" + workspace + node.getPath();
     return link;
   }
 
   public String getAuthor(Node node) throws Exception {
-
-    return node.getProperty("exo:owner").getValue().getString();
+    if(node.hasProperty("exo:owner")) {
+      String ownerId = node.getProperty("exo:owner").getValue().getString();
+      OrganizationService organizationService = getApplicationComponent(OrganizationService.class);
+      UserProfileHandler handler = organizationService.getUserProfileHandler();
+      UserProfile userProfile = handler.findUserProfileByName(ownerId);
+      return userProfile.getUserInfoMap().get("user.name.given");
+    }
+    return null;    
   }
-
+  
   public String getCreatedDate(Node node) throws Exception {
-    return node.hasProperty("exo:dateCreated") ? node.getProperty("exo:dateCreated").getValue()
-        .getString() : null;
+    if(node.hasProperty("exo:dateCreated")) {
+      Calendar calendar = node.getProperty("exo:dateCreated").getValue().getDate();
+      return dateFormatter.format(calendar.getTime());
+    }
+    return null;    
   }
 
-  public String getModifiedDate(Node node) throws Exception {
-    return node.hasProperty("exo:dateModified") ? node.getProperty("exo:dateModified").getValue()
-        .getString() : null;
+  public String getModifiedDate(Node node) throws Exception {    
+    if(node.hasProperty("exo:dateModified")) {
+      Calendar calendar = node.getProperty("exo:dateModified").getValue().getDate();
+      return dateFormatter.format(calendar.getTime());
+    }
+    return null;    
   }
 
   public String getContentType(Node node) {
@@ -130,6 +152,30 @@ public class UIContentListPresentation extends UIContainer {
   }
 
   public String getContentSize(Node node) {
+    return null;
+  }
+
+  public String getIllustrativeImage(Node node) {
+    return null;
+  }
+
+  public List<String> getCategories(Node node) {
+    return null;
+  }
+
+  public List<String> getTags(Node node) {
+    return null;
+  }
+
+  public float getVotingRate(Node node) { 
+    return 0; 
+  }
+
+  public int getNumberOfComments(Node node) {
+    return 0;
+  }    
+
+  public List<Node> getRelatedContents(Node node) {
     return null;
   }
 
