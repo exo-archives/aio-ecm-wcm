03c7aeccb495be49c4ac3d86d5e8eb975df56898
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2008-11-07 08:27:37 +0000

    WCM-391

--- portlets/iweb/src/main/webapp/javascript/eXo/CheckShowAdminToolBar.js
+++ portlets/iweb/src/main/webapp/javascript/eXo/CheckShowAdminToolBar.js
@@ -1,9 +1,11 @@
 function isSiteToolbarShowed() {
 	var root = document.getElementById("admintoolbar");
-	var siteAdminToolbar = eXo.core.DOMUtil.findDescendantById(root, "ViewMode");
-	if (siteAdminToolbar) {
-		var uiBanner = document.getElementById("UIBannerPortlet");
-		var loginContent = eXo.core.DOMUtil.findFirstDescendantByClass(uiBanner, "div", "LoginContent");
-		loginContent.style.top = "70px";
+	if (root){
+		var siteAdminToolbar = eXo.core.DOMUtil.findDescendantById(root, "ViewMode");
+		if (siteAdminToolbar) {
+			var uiBanner = document.getElementById("UIBannerPortlet");
+			var loginContent = eXo.core.DOMUtil.findFirstDescendantByClass(uiBanner, "div", "LoginContent");
+			loginContent.style.top = "70px";
+		}
 	}
 }
