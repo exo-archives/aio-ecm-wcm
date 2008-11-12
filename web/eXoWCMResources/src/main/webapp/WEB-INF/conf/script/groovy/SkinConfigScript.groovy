64771d772e8f299cfa60d2c297bf13f443087f99
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2008-11-12 03:18:48 +0000

    refactor CSS

--- web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,3 +1,3 @@
 SkinService.addPortalSkin("ECMSkin", "Default", "/eXoWCMResources/skin/DefaultSkin/Stylesheet.css", ServletContext,true);
-SkinService.addPortalSkin("ECMSkin", "Mac", "/eXoWCMResources/skin/MacSkin/Stylesheet.css", ServletContext,true);
-SkinService.addPortalSkin("ECMSkin", "Vista", "/eXoWCMResources/skin/VistaSkin/Stylesheet.css", ServletContext,true);
+SkinService.addPortalSkin("ECMSkin", "Mac", "/eXoWCMResources/skin/DefaultSkin/Stylesheet.css", ServletContext,true);
+SkinService.addPortalSkin("ECMSkin", "Vista", "/eXoWCMResources/skin/DefaultSkin/Stylesheet.css", ServletContext,true);
