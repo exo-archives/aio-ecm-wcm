057128afe653ae5e1847226816d1bedbcf441f4f
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-11-25 07:44:52 +0000

    WCM-480

---
+++ web/wcmportal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,23 @@
+/***************************Login Form skin**************************************************/
+SkinService.addSkin(
+    "web/login",
+    "Default",
+    "/portal/login/skin/Stylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "web/login",
+    "Vista",
+    "/portal/login/skin/Stylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "web/login",
+    "Mac",
+    "/portal/login/skin/Stylesheet.css",
+    ServletContext
+) ;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
