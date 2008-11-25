e77506b736523c4f891cb4486adebe8568c10005
Do Ngoc Anh <anh.do@exoplatform.com>
2008-11-25 08:55:31 +0000

    improve for login webcontent

--- web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
+++ web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
@@ -85,36 +85,3 @@ function getHostName() {
 	var parentLocation = window.parent.location;
 	return parentLocation.href.substring(0, parentLocation.href.indexOf(parentLocation.pathname));
 }
-
-function validateUser() {
-
-	var user = eXo.env.portal.userName;
-	var rootObj = document.getElementById("classic-access");
-	var loginContentObj = eXo.core.DOMUtil.findFirstDescendantByClass(rootObj, "div", "LoginContent");
-	var welcomeObj = eXo.core.DOMUtil.findFirstDescendantByClass(rootObj, "div", "Welcome");
-	var languageObj = eXo.core.DOMUtil.findFirstDescendantByClass(rootObj, "a", "LanguageIcon");
-	var logXXXObj = eXo.core.DOMUtil.findPreviousElementByTagName(languageObj, "a");
-
-	if (user != "null") {
-		welcomeObj.style.display = "block";
-		welcomeObj.innerHTML = "Welcome: <span>" + user + "</span>";		
-		logXXXObj.innerHTML = "Logout";
-		if (eXo.core.DOMUtil.hasClass(logXXXObj, "LoginIcon")) {
-			eXo.core.DOMUtil.removeClass(logXXXObj, "LoginIcon");
-			eXo.core.DOMUtil.addClass(logXXXObj, "LogoutIcon");
-		}
-		logXXXObj.onclick = function() { eXo.portal.logout(); }
-	} else {
-		welcomeObj.style.display = "none";
-		if (eXo.core.DOMUtil.hasClass(logXXXObj, "LogoutIcon")) {
-			eXo.core.DOMUtil.removeClass(logXXXObj, "LogoutIcon");
-			eXo.core.DOMUtil.addClass(logXXXObj, "LoginIcon");
-		}
-		logXXXObj.innerHTML = "Login";
-		logXXXObj.onclick = function() { if(document.getElementById('UIMaskWorkspace')) ajaxGet(eXo.env.server.createPortalURL('UIPortal', 'ShowLoginForm', true)); };
-	}
-
-	languageObj.onclick = function () { if(document.getElementById('UIMaskWorkspace')) ajaxGet(eXo.env.server.createPortalURL('UIPortal', 'ChangeLanguage', true)); }
-}
-
-eXo.core.Browser.addOnLoadCallback("validateUser", validateUser);
