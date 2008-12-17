a9fc3f6157ee4ad462b06c57ca088d761aba11ae
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2008-12-17 09:40:49 +0000

    WCM-494

--- component/search/src/main/java/org/exoplatform/services/wcm/search/QueryCriteria.java
+++ component/search/src/main/java/org/exoplatform/services/wcm/search/QueryCriteria.java
@@ -21,77 +21,245 @@ import java.util.Calendar;
 
 /**
  * Created by The eXo Platform SAS
- * Author : Hoa Pham	
- *          hoa.pham@exoplatform.com
- * Oct 7, 2008  
+ * Author : Hoa Pham
+ * hoa.pham@exoplatform.com
+ * Oct 7, 2008
  */
 /*
  * This is query criteria for SiteSearch service. Base on search criteria, SiteSearch service 
  * can easy create query statement to search.
  * */
 public class QueryCriteria {  
+  
+  /** The site name. */
   private String siteName;
+  
+  /** The categories. */
   private String[] categories;
+  
+  /** The tags. */
   private String[] tags;
+  
+  /** The start publication date. */
   private Calendar startPublicationDate;
+  
+  /** The end publication date. */
   private Calendar endPublicationDate;
+  
+  /** The authors. */
   private String[] authors;  
+  
+  /** The content types. */
   private String[] contentTypes;
+  
+  /** The mime types. */
   private String[] mimeTypes;
+  
+  /** The created date. */
   private Calendar createdDate;
+  
+  /** The last modified date. */
   private Calendar lastModifiedDate;
+  
+  /** The search webpage. */
   private boolean searchWebpage = true;
+  
+  /** The search document. */
   private boolean searchDocument = true;
+  
+  /** The keyword. */
   private String keyword;
   
+  /**
+   * Gets the site name.
+   * 
+   * @return the site name
+   */
   public String getSiteName() { return siteName; }
+  
+  /**
+   * Sets the site name.
+   * 
+   * @param siteName the new site name
+   */
   public void setSiteName(String siteName) { this.siteName = siteName; }
   
+  /**
+   * Gets the categories.
+   * 
+   * @return the categories
+   */
   public String[] getCategories() { return categories; }
+  
+  /**
+   * Sets the categories.
+   * 
+   * @param categories the new categories
+   */
   public void setCategories(String[] categories) { this.categories = categories; }
   
+  /**
+   * Gets the tags.
+   * 
+   * @return the tags
+   */
   public String[] getTags() { return tags; }
+  
+  /**
+   * Sets the tags.
+   * 
+   * @param tags the new tags
+   */
   public void setTags(String[] tags) { this.tags = tags; }
 
+  /**
+   * Gets the start publication date.
+   * 
+   * @return the start publication date
+   */
   public Calendar getStartPublicationDate() { return startPublicationDate; }
 
+  /**
+   * Sets the start publication date.
+   * 
+   * @param startPublicationDate the new start publication date
+   */
   public void setStartPublicationDate(Calendar startPublicationDate) { 
     this.startPublicationDate = startPublicationDate; 
   }
 
+  /**
+   * Gets the end publication date.
+   * 
+   * @return the end publication date
+   */
   public Calendar getEndPublicationDate() {
     return endPublicationDate;
   }
 
+  /**
+   * Sets the end publication date.
+   * 
+   * @param endPublicationDate the new end publication date
+   */
   public void setEndPublicationDate(Calendar endPublicationDate) {
     this.endPublicationDate = endPublicationDate;
   }
 
+  /**
+   * Gets the authors.
+   * 
+   * @return the authors
+   */
   public String[] getAuthors() { return authors; }
+  
+  /**
+   * Sets the authors.
+   * 
+   * @param authors the new authors
+   */
   public void setAuthors(String[] authors) { this.authors = authors; }
+  
+  /**
+   * Gets the content types.
+   * 
+   * @return the content types
+   */
   public String[] getContentTypes() { return contentTypes; }
+  
+  /**
+   * Sets the content types.
+   * 
+   * @param contentTypes the new content types
+   */
   public void setContentTypes(String[] contentTypes) { this.contentTypes = contentTypes; }
 
+  /**
+   * Gets the mime types.
+   * 
+   * @return the mime types
+   */
   public String[] getMimeTypes() { return mimeTypes; }
+  
+  /**
+   * Sets the mime types.
+   * 
+   * @param mimeTypes the new mime types
+   */
   public void setMimeTypes(String[] mimeTypes) { this.mimeTypes = mimeTypes; }
 
+  /**
+   * Gets the created date.
+   * 
+   * @return the created date
+   */
   public Calendar getCreatedDate() { return createdDate; }
+  
+  /**
+   * Sets the created date.
+   * 
+   * @param createdDate the new created date
+   */
   public void setCreatedDate(Calendar createdDate) { this.createdDate = createdDate; }
 
+  /**
+   * Gets the last modified date.
+   * 
+   * @return the last modified date
+   */
   public Calendar getLastModifiedDate() { return lastModifiedDate; }
+  
+  /**
+   * Sets the last modified date.
+   * 
+   * @param lastModifiedDate the new last modified date
+   */
   public void setLastModifiedDate(Calendar lastModifiedDate) {
     this.lastModifiedDate = lastModifiedDate;
   }
   
+  /**
+   * Gets the keyword.
+   * 
+   * @return the keyword
+   */
   public String getKeyword() { return this.keyword; }
+  
+  /**
+   * Sets the keyword.
+   * 
+   * @param s the new keyword
+   */
   public void setKeyword(String s) { this.keyword = s; }
   
+  /**
+   * Checks if is search webpage.
+   * 
+   * @return true, if is search webpage
+   */
   public boolean isSearchWebpage() { return searchWebpage;}
+  
+  /**
+   * Sets the search webpage.
+   * 
+   * @param searchWebpage the new search webpage
+   */
   public void setSearchWebpage(boolean searchWebpage) { 
     this.searchWebpage = searchWebpage;
   }
   
+  /**
+   * Checks if is search document.
+   * 
+   * @return true, if is search document
+   */
   public boolean isSearchDocument() { return searchDocument;}
+  
+  /**
+   * Sets the search document.
+   * 
+   * @param searchDocument the new search document
+   */
   public void setSearchDocument(boolean searchDocument) {
     this.searchDocument = searchDocument;
   }
