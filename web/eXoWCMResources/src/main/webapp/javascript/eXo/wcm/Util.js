7669cf60e815b1dbf9aac70a4e5fe2184b7b52af
Do Ngoc Anh <anh.do@exoplatform.com>
2009-02-24 06:40:20 +0000

    Improve ui for CLV

--- web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
+++ web/eXoWCMResources/src/main/webapp/javascript/eXo/wcm/Util.js
@@ -623,3 +623,19 @@ function setHiddenValue() {
 		document.getElementById(eXo.core.Browser.chkRadioId).checked = true;
 	}
 }
+
+function showHideOrderBy() {
+	var formObj = document.getElementById('UIViewerManagementForm');
+	var viewerModeObj = formObj['ViewerMode'];
+	var orderXXX = eXo.core.DOMUtil.findDescendantsByClass(formObj, 'tr', 'OrderBlock');			
+	viewerModeObj[0].onclick = function() {
+		for (var i = 0; i < orderXXX.length; i++) {
+			orderXXX[i].style.display = '';
+		}
+	}
+	viewerModeObj[1].onclick = function() {
+		for (var i = 0; i < orderXXX.length; i++) {
+			orderXXX[i].style.display = 'none';
+		}
+	}
+}  
