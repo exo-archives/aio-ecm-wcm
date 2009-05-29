fdc68cd0c76f5efaf1dde07201045d54b9169fcb
Mai Van Ha <ha.mai@exoplatform.com>
2009-05-29 01:54:10 +0000

    Create newsletter portlet

---
+++ portlets/newsletter/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,41 @@
+SkinService.addSkin(
+    "newsletter/NewsletterManagementPortlet",
+    "Default",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterManagementPortlet",
+    "Mac",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterManagementPortlet",
+    "Vista",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Default",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Mac",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Vista",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
