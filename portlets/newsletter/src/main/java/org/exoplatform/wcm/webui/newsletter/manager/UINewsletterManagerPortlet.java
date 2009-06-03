37a9c05b7c9c18d2d6bf6c8571e3e9701a3e10b6
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2009-06-03 03:19:22 +0000

    WCM-1454: [DEV] - Newsletter viewer portlet

---
+++ portlets/newsletter/src/main/java/org/exoplatform/wcm/webui/newsletter/manager/UINewsletterManagerPortlet.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.wcm.webui.newsletter.manager;
+
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIPortletApplication;
+import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          ngoc.tran@exoplatform.com
+ * May 22, 2009  
+ */
+@ComponentConfig (
+		lifecycle = UIApplicationLifecycle.class
+)
+public class UINewsletterManagerPortlet extends UIPortletApplication {
+	private boolean isRenderUICategories = true;
+	private boolean isRenderUICategory = true;
+	private boolean isRenderUISubscription = true;
+	
+	public UINewsletterManagerPortlet() throws Exception {
+		System.out.println("\n\n\n\n-------------->run new letter manager portlet");
+		this.addChild(UICategories.class, null, null).setRendered(isRenderUICategories);
+	}
+}
---
+++ portlets/newsletter/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,20 @@
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Default",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Vista",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "newsletter/NewsletterViewerPortlet",
+    "Mac",
+    "/newsletter/skin/DefaultSkin/DefaultStylesheet.css",
+    ServletContext
+) ;
