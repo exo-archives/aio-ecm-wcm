421f266aa0dc8a9c807f6865bb703fcd869e169d
Benjamin  Paillereau <benjamin.paillereau@exoplatform.com>
2009-08-18 14:33:51 +0000

    REOPENED - issue WCM-519: Allow user vote/comment in SCV http://jira.exoplatform.org/browse/WCM-519

--- web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ web/eXoWCMResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,3 +1,4 @@
 /* External libraries */
 JavascriptService.addJavascript("eXo.wcm.Util", "/javascript/eXo/wcm/Util.js", ServletContext);
 JavascriptService.addJavascript("eXo.wcm.Util", "/javascript/eXo/wcm/Navigation.js", ServletContext);
+JavascriptService.addJavascript("eXo.wcm.Util", "/javascript/eXo/wcm/Services.js", ServletContext);
