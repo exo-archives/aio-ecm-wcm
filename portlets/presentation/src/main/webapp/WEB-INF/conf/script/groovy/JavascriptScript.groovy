f29155d88b11161cb899e485ce2645ed3f6d5866
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2009-10-15 03:25:43 +0000

    WCM-2079: Remove Show Help or link to move between steps while adding new Content

--- portlets/presentation/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ portlets/presentation/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,3 +1,2 @@
 /* External libraries */
-JavascriptService.addJavascript("eXo.webpresentation.Utils", "/javascript/eXo/webpresentation/Utils.js", ServletContext);
 JavascriptService.addJavascript("eXo.SiteAdministration.ShowAdminToolbar", "/javascript/eXo/SiteAdministration/ShowAdminToolbar.js", ServletContext);
