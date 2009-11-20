eb9c9e8d9958b798965ddd34dff1adeedc7fbb15
Mai Van Ha <ha.mai@exoplatform.com>
2009-11-20 03:50:55 +0000

    WCM-2139 Dialog which confirms whether a subscribe is deleted displayed before the alert message to request user to select a subscribe.

--- portlets/newsletter/src/main/webapp/javascript/eXo/wcm/newsletter/NewsletterManager/UINewsletterUtil.js
+++ portlets/newsletter/src/main/webapp/javascript/eXo/wcm/newsletter/NewsletterManager/UINewsletterUtil.js
@@ -10,4 +10,17 @@ UINewsletterUtil.prototype.checkAllSelected = function(checkBox) {
 	}
 };
 
+UINewsletterUtil.prototype.checkBeforeDelete = function(checkBox, checkMess, confirmMess) {
+	var isChecked = false;
+	var elements = document.getElementById(checkBox).form.elements;
+	for(var i = 0; i < elements.length; i ++){
+		if(elements[i].type == "checkbox" && elements[i].checked == true) isChecked = true;
+	}
+	if(isChecked == false){
+		alert(checkMess);
+		return false;
+	}
+	return confirm(confirmMess);
+}
+
 eXo.ecm.UINewsletterUtil = new UINewsletterUtil();
