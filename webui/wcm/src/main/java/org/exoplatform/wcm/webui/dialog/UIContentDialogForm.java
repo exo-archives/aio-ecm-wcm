1bc5ef690ee1eb413acc3955a7055a50d192b4cb
Tran Nguyen Ngoc <ngoc.tran@exoplatform.com>
2009-11-27 01:52:49 +0000

    WCM-2398:Not show document content exactly after selecting a document having Kofax template

--- webui/wcm/src/main/java/org/exoplatform/wcm/webui/dialog/UIContentDialogForm.java
+++ webui/wcm/src/main/java/org/exoplatform/wcm/webui/dialog/UIContentDialogForm.java
@@ -17,29 +17,41 @@
 package org.exoplatform.wcm.webui.dialog;
 
 import java.security.AccessControlException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.jcr.AccessDeniedException;
 import javax.jcr.ItemExistsException;
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Value;
 import javax.jcr.lock.LockException;
 import javax.jcr.version.VersionException;
 
 import org.exoplatform.ecm.resolver.JCRResourceResolver;
 import org.exoplatform.ecm.webui.form.DialogFormActionListeners;
 import org.exoplatform.ecm.webui.form.UIDialogForm;
+import org.exoplatform.ecm.webui.selector.UISelectable;
+import org.exoplatform.ecm.webui.tree.selectone.UIOneTaxonomySelector;
 import org.exoplatform.ecm.webui.utils.DialogFormUtil;
 import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.resolver.ResourceResolver;
+import org.exoplatform.services.cms.BasePath;
 import org.exoplatform.services.cms.CmsService;
 import org.exoplatform.services.cms.JcrInputProperty;
 import org.exoplatform.services.cms.impl.DMSConfiguration;
+import org.exoplatform.services.cms.impl.DMSRepositoryConfiguration;
 import org.exoplatform.services.cms.templates.TemplateService;
 import org.exoplatform.services.ecm.publication.PublicationPlugin;
 import org.exoplatform.services.ecm.publication.PublicationService;
+import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.core.ManageableRepository;
+import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
 import org.exoplatform.services.wcm.core.NodeLocation;
 import org.exoplatform.services.wcm.publication.PublicationDefaultStates;
 import org.exoplatform.services.wcm.publication.lifecycle.stageversion.StageAndVersionPublicationConstant;
@@ -50,10 +62,16 @@ import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.webui.core.UIPopupComponent;
 import org.exoplatform.webui.core.UIPopupContainer;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
+import org.exoplatform.webui.event.Event.Phase;
+import org.exoplatform.webui.form.UIFormInput;
+import org.exoplatform.webui.form.UIFormInputBase;
+import org.exoplatform.webui.form.UIFormMultiValueInputSet;
+import org.exoplatform.webui.form.UIFormStringInput;
 
 /**
  * Created by The eXo Platform SAS
@@ -64,6 +82,9 @@ import org.exoplatform.webui.event.EventListener;
 @ComponentConfig (
     lifecycle = UIFormLifecycle.class,
     events = {
+      @EventConfig(listeners = UIContentDialogForm.AddActionListener.class, phase = Phase.DECODE),
+      @EventConfig(listeners = UIContentDialogForm.RemoveReferenceActionListener.class, 
+                   confirm = "DialogFormField.msg.confirm-delete", phase = Phase.DECODE),
       @EventConfig(listeners = UIContentDialogForm.SaveDraftActionListener.class),
       @EventConfig(listeners = UIContentDialogForm.FastPublishActionListener.class),
       @EventConfig(listeners = UIContentDialogForm.PreferencesActionListener.class),
@@ -71,12 +92,30 @@ import org.exoplatform.webui.event.EventListener;
       @EventConfig(listeners = DialogFormActionListeners.RemoveDataActionListener.class)
     }
 )
-public class UIContentDialogForm extends UIDialogForm {
+public class UIContentDialogForm extends UIDialogForm  implements UIPopupComponent, UISelectable {
 
 	public static final String CONTENT_DIALOG_FORM_POPUP_WINDOW = "UIContentDialogFormPopupWindow";
-	
+	public static final String FIELD_TAXONOMY = "categories";
+	public static final String TAXONOMY_CONTENT_POPUP_WINDOW = "UIContentPopupWindow";
 	private NodeLocation webcontentNodeLocation;
-	
+	private List<String> listTaxonomy = new ArrayList<String>();
+  private List<String> listTaxonomyName = new ArrayList<String>();
+  
+  public List<String> getListTaxonomy() {
+    return listTaxonomy;
+  }
+  
+  public List<String> getlistTaxonomyName() {
+    return listTaxonomyName;
+  }
+  
+  public void setListTaxonomy(List<String> listTaxonomyNew) {
+    listTaxonomy = listTaxonomyNew;
+  }
+  
+  public void setListTaxonomyName(List<String> listTaxonomyNameNew) {
+    listTaxonomyName = listTaxonomyNameNew;
+  }
 	private Class<? extends UIContentDialogPreference> preferenceComponent;
 	
 	public NodeLocation getWebcontentNodeLocation() {
@@ -228,6 +267,10 @@ public class UIContentDialogForm extends UIDialogForm {
           webContentNode.checkout();
         }
       	List<UIComponent> inputs = contentDialogForm.getChildren();
+      	if(contentDialogForm.checkCategories(contentDialogForm)) {
+      	  Utils.createPopupMessage(contentDialogForm, "UIContentDialogForm.msg.non-categories", null, ApplicationMessage.WARNING);
+          return;
+      	}
       	Map<String, JcrInputProperty> inputProperties = DialogFormUtil.prepareMap(inputs, contentDialogForm.getInputProperties());
         CmsService cmsService = contentDialogForm.getApplicationComponent(CmsService.class);
         cmsService.storeNode(contentDialogForm.contentType, contentDialogForm.getNode().getParent(), inputProperties, contentDialogForm.isAddNew, contentDialogForm.repositoryName);
@@ -250,6 +293,48 @@ public class UIContentDialogForm extends UIDialogForm {
       }
       Utils.closePopupWindow(contentDialogForm, CONTENT_DIALOG_FORM_POPUP_WINDOW);      
     }
+  }
+  
+  private boolean checkCategories(UIContentDialogForm contentDialogForm) {
+    String categoriesPath = "";
+    String[] categoriesPathList = null;
+    int index = 0;
+    if (contentDialogForm.isReference) {
+      UIFormMultiValueInputSet uiSet = contentDialogForm.getChild(UIFormMultiValueInputSet.class);
+      if((uiSet != null) && (uiSet.getName() != null) && uiSet.getName().equals(FIELD_TAXONOMY)) {
+        List<UIComponent> listChildren = uiSet.getChildren();         
+        for (UIComponent component : listChildren) {
+          UIFormStringInput uiStringInput = (UIFormStringInput)component;          
+          if(uiStringInput.getValue() != null) {
+            String value = uiStringInput.getValue().trim();            
+            categoriesPath += value + ",";
+          }
+        }
+        if (categoriesPath != null && categoriesPath.length() > 0) {
+          try {
+            if (categoriesPath.endsWith(",")) {
+              categoriesPath = categoriesPath.substring(0, categoriesPath.length()-1).trim();
+              if(categoriesPath.trim().length() == 0) {
+                return true;
+              }
+            }
+            categoriesPathList = categoriesPath.split(",");
+            if ((categoriesPathList == null) || (categoriesPathList.length == 0)) {
+              return true;
+            }
+            for (String categoryPath : categoriesPathList) {
+              index = categoryPath.indexOf("/");
+              if (index < 0) {
+                return true;
+              }
+            }
+          } catch (Exception e) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
   
   /**
@@ -276,6 +361,10 @@ public class UIContentDialogForm extends UIDialogForm {
           webContentNode.checkout();
         }
       	List<UIComponent> inputs = contentDialogForm.getChildren();
+      	if(contentDialogForm.checkCategories(contentDialogForm)) {
+          Utils.createPopupMessage(contentDialogForm, "UIContentDialogForm.msg.non-categories", null, ApplicationMessage.WARNING);
+          return;
+        }
       	Map<String, JcrInputProperty> inputProperties = DialogFormUtil.prepareMap(inputs, contentDialogForm.getInputProperties());
         CmsService cmsService = contentDialogForm.getApplicationComponent(CmsService.class);
         cmsService.storeNode(contentDialogForm.contentType, contentDialogForm.getNode().getParent(), inputProperties, contentDialogForm.isAddNew, contentDialogForm.repositoryName);
@@ -310,4 +399,97 @@ public class UIContentDialogForm extends UIDialogForm {
 	  }
   }
   
+  static public class AddActionListener extends EventListener<UIContentDialogForm> {
+
+    /* (non-Javadoc)
+     * @see org.exoplatform.webui.event.EventListener#execute(org.exoplatform.webui.event.Event)
+     */
+    public void execute(Event<UIContentDialogForm> event) throws Exception {
+      UIContentDialogForm contentDialogForm = event.getSource();
+      String clickedField = event.getRequestContext().getRequestParameter(OBJECTID);
+      if (contentDialogForm.isReference) {
+        UIApplication uiApp = contentDialogForm.getAncestorOfType(UIApplication.class);
+        try{
+          UIFormMultiValueInputSet uiSet = contentDialogForm.getChildById(FIELD_TAXONOMY);
+          if((uiSet != null) && (uiSet.getName() != null) && uiSet.getName().equals(FIELD_TAXONOMY)) {
+            if ((clickedField != null) && (clickedField.equals(FIELD_TAXONOMY))){
+              NodeHierarchyCreator nodeHierarchyCreator = 
+                contentDialogForm.getApplicationComponent(NodeHierarchyCreator.class);
+              String repository = contentDialogForm.repositoryName;
+              DMSConfiguration dmsConfiguration = contentDialogForm.getApplicationComponent(DMSConfiguration.class);
+              DMSRepositoryConfiguration repositoryConfiguration = dmsConfiguration.getConfig(repository);
+              String workspaceName = repositoryConfiguration.getSystemWorkspace();
+              UIOneTaxonomySelector uiOneTaxonomySelector = 
+                contentDialogForm.createUIComponent(UIOneTaxonomySelector.class, null, null);
+              if(uiSet.getValue().size() == 0) uiSet.setValue(new ArrayList<Value>()); 
+              String rootTreePath = nodeHierarchyCreator.getJcrPath(BasePath.TAXONOMIES_TREE_STORAGE_PATH);
+              RepositoryService repositoryService = (RepositoryService)contentDialogForm.getApplicationComponent(RepositoryService.class);
+              ManageableRepository manageableRepository = repositoryService.getRepository(repository);
+              Session session = Utils.getSessionProvider(contentDialogForm).getSession(workspaceName, manageableRepository);
+              Node rootTree = (Node) session.getItem(rootTreePath);
+              NodeIterator childrenIterator = rootTree.getNodes();
+              while (childrenIterator.hasNext()) {
+                Node childNode = childrenIterator.nextNode();
+                rootTreePath = childNode.getPath();
+                break;
+              }
+              uiOneTaxonomySelector.setRootNodeLocation(repository, workspaceName, rootTreePath);
+              uiOneTaxonomySelector.setExceptedNodeTypesInPathPanel(new String[] {"exo:symlink"});
+              uiOneTaxonomySelector.init(Utils.getSystemProvider(contentDialogForm));
+              String param = "returnField=" + FIELD_TAXONOMY;
+              uiOneTaxonomySelector.setSourceComponent(contentDialogForm, new String[]{param});
+              Utils.createPopupWindow(contentDialogForm, uiOneTaxonomySelector, TAXONOMY_CONTENT_POPUP_WINDOW, 700, 450);
+            }
+          }
+        }catch (AccessDeniedException accessDeniedException) {
+          uiApp.addMessage(new ApplicationMessage("UIContentDialogForm.msg.access-denied", null, 
+                                                  ApplicationMessage.WARNING));
+          event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+          return;
+        } catch (Exception e) {
+          uiApp.addMessage(new ApplicationMessage("UIContentDialogForm.msg.exception", null, 
+              ApplicationMessage.WARNING));
+          event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+          return;
+        }
+      } else {
+        event.getRequestContext().addUIComponentToUpdateByAjax(contentDialogForm);
+      }
+    }
+  }
+
+  public void activate() throws Exception {
+  }
+
+  public void deActivate() throws Exception {
+  }
+
+  @SuppressWarnings("unchecked")
+  public void doSelect(String selectField, Object value) throws Exception {
+    isUpdateSelect = true;    
+    UIFormInput formInput = getUIInput(selectField);
+    if(formInput instanceof UIFormInputBase) {
+      ((UIFormInputBase)formInput).setValue(value.toString());
+    }else if(formInput instanceof UIFormMultiValueInputSet) {
+      UIFormMultiValueInputSet  inputSet = (UIFormMultiValueInputSet) formInput;            
+      String valueTaxonomy = String.valueOf(value).trim();
+      List taxonomylist = inputSet.getValue();
+      if (!taxonomylist.contains(valueTaxonomy)) {
+        listTaxonomy.add(valueTaxonomy);
+        listTaxonomyName.add(valueTaxonomy);
+        taxonomylist.add(valueTaxonomy);
+      }      
+      inputSet.setValue(taxonomylist);
+    }
+  }
+  
+  static public class RemoveReferenceActionListener extends EventListener<UIContentDialogForm> {
+    public void execute(Event<UIContentDialogForm> event) throws Exception {
+      UIContentDialogForm contentDialogForm = event.getSource();
+      contentDialogForm.isRemovePreference = true;
+      String fieldName = event.getRequestContext().getRequestParameter(OBJECTID);
+      contentDialogForm.getUIStringInput(fieldName).setValue(null);
+      event.getRequestContext().addUIComponentToUpdateByAjax(contentDialogForm);
+    }
+  }
 }
