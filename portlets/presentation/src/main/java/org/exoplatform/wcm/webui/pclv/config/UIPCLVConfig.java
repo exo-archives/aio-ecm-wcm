b9a44e2f0ef8768dcaa90a093a24c1000a6265a8
Tran Nguyen Ngoc <ngoc.tran@exoplatform.com>
2009-12-02 04:50:48 +0000

    WCM-2412:Double taxonomy when config a PCLV

--- portlets/presentation/src/main/java/org/exoplatform/wcm/webui/pclv/config/UIPCLVConfig.java
+++ portlets/presentation/src/main/java/org/exoplatform/wcm/webui/pclv/config/UIPCLVConfig.java
@@ -308,8 +308,7 @@ public class UIPCLVConfig extends UIForm implements UISelectable {
     for(Node taxonomyNode : taxonomyNodes) {
     	Node portalNode = livePortalManagerService.getLivePortalByChild(taxonomyNode);
     	if (portalNode != null)
-    		taxonomyTrees.add(new SelectItemOption<String>(taxonomyNode.getName(), taxonomyNode.getName()));
-      taxonomyTrees.add(new SelectItemOption<String>(taxonomyNode.getName(), taxonomyNode.getName()));
+			taxonomyTrees.add(new SelectItemOption<String>(taxonomyNode.getName(), taxonomyNode.getName()));
     }
     return taxonomyTrees;
   }
