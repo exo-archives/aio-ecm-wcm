6d63c33ab99420312cb07f83cfcf617ec2983245
Haikel THAMRI <haikel.thamri@gmail.com>
2010-01-27 18:00:35 +0000

    WCM-2489 : Modify the cron to support the Authoring Plugin

--- exo.ecm.wcm.extensions.ext.publication/src/main/java/org/exoplatform/services/wcm/extensions/scheduler/impl/ExportContentJob.java
+++ exo.ecm.wcm.extensions.ext.publication/src/main/java/org/exoplatform/services/wcm/extensions/scheduler/impl/ExportContentJob.java
@@ -11,12 +11,20 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.Session;
+import javax.jcr.Value;
+import javax.jcr.ValueFactory;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
@@ -31,15 +39,18 @@ import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.wcm.extensions.publication.lifecycle.authoring.AuthoringPublicationConstant;
 import org.exoplatform.services.wcm.extensions.security.SHAMessageDigester;
+import org.exoplatform.services.wcm.publication.PublicationDefaultStates;
 import org.exoplatform.services.wcm.publication.lifecycle.stageversion.StageAndVersionPublicationConstant;
+import org.exoplatform.services.wcm.publication.lifecycle.stageversion.config.VersionData;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
 /**
- * Created by The eXo Platform MEA Author : 
- * haikel.thamri@exoplatform.com
+ * Created by The eXo Platform MEA Author : haikel.thamri@exoplatform.com
  */
 public class ExportContentJob implements Job {
     private static final Log log = ExoLogger.getLogger(ExportContentJob.class);
@@ -49,6 +60,8 @@ public class ExportContentJob implements Job {
     private static final String MIX_TARGET_WORKSPACE = "mix:targetWorkspace";
 
     private static final String URL = "http://www.w3.org/2001/XMLSchema";
+
+    private static final String START_TIME_PROPERTY = "publication:startPublishedDate".intern();
 
     private static String fromState = null;
 
@@ -104,11 +117,10 @@ public class ExportContentJob implements Job {
 	    ManageableRepository manageableRepository = repositoryService_.getRepository(repository);
 	    session = sessionProvider.getSession(workspace, manageableRepository);
 	    QueryManager queryManager = session.getWorkspace().getQueryManager();
-
+	    boolean isExported = false;
 	    Query query = queryManager.createQuery("select * from nt:base where publication:currentState='" + fromState + "' and jcr:path like '"
 		    + contentPath + "/%'", Query.SQL);
 	    QueryResult queryResult = query.execute();
-
 	    if (queryResult.getNodes().getSize() > 0) {
 
 		File exportFolder = new File(localTempDir);
@@ -124,84 +136,148 @@ public class ExportContentJob implements Job {
 		xmlsw.writeNamespace("xs", URL);
 		for (NodeIterator iter = queryResult.getNodes(); iter.hasNext();) {
 		    Node node_ = iter.nextNode();
-		    if (node_.canAddMixin(MIX_TARGET_PATH))
-			node_.addMixin(MIX_TARGET_PATH);
-		    node_.setProperty(MIX_TARGET_PATH, node_.getPath());
+		    if (node_.hasProperty(START_TIME_PROPERTY)) {
 
-		    if (node_.canAddMixin(MIX_TARGET_WORKSPACE))
-			node_.addMixin(MIX_TARGET_WORKSPACE);
-		    node_.setProperty(MIX_TARGET_WORKSPACE, workspace);
+			SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy - HH:mm");
+			Date now = Calendar.getInstance().getTime();
+			Date nodeDate = node_.getProperty(START_TIME_PROPERTY).getDate().getTime();
+			ValueFactory valueFactory = session.getValueFactory();
+			if (now.compareTo(nodeDate) >= 0) {
+			    if (node_.canAddMixin(MIX_TARGET_PATH))
+				node_.addMixin(MIX_TARGET_PATH);
+			    node_.setProperty(MIX_TARGET_PATH, node_.getPath());
 
-		    HashMap<String, String> publicationContext = new HashMap<String, String>();
-		    publicationContext.put(StageAndVersionPublicationConstant.CURRENT_REVISION_NAME, node_.getName());
-		    session.save();
-		    publicationService_.changeState(node_, toState, publicationContext);
+			    if (node_.canAddMixin(MIX_TARGET_WORKSPACE))
+				node_.addMixin(MIX_TARGET_WORKSPACE);
+			    node_.setProperty(MIX_TARGET_WORKSPACE, workspace);
+			    node_.setProperty(StageAndVersionPublicationConstant.LIVE_REVISION_PROP, valueFactory.createValue(""));
+			    Map<String, VersionData> revisionsMap = getRevisionData(node_);
+			    node_.setProperty(StageAndVersionPublicationConstant.CURRENT_STATE, PublicationDefaultStates.PUBLISHED);
 
-		    bos = new ByteArrayOutputStream();
-		    session.exportSystemView(node_.getPath(), bos, false, false);
-		    xmlsw.writeStartElement("xs", "content", URL);
-		    xmlsw.writeCData(bos.toString());
-		    xmlsw.writeEndElement();
+			    VersionData versionData = revisionsMap.get(node_.getUUID());
+			    if (versionData != null) {
+				versionData.setAuthor(node_.getSession().getUserID());
+				versionData.setState(PublicationDefaultStates.PUBLISHED);
+			    } else {
+				versionData = new VersionData(node_.getUUID(), PublicationDefaultStates.PUBLISHED, node_.getSession().getUserID());
+			    }
+			    revisionsMap.put(node_.getUUID(), versionData);
+			    addRevisionData(node_, revisionsMap.values());
+			    session.save();
+			    bos = new ByteArrayOutputStream();
+			    session.exportSystemView(node_.getPath(), bos, false, false);
+			    isExported = true;
+			    xmlsw.writeStartElement("xs", "content", URL);
+			    xmlsw.writeCData(bos.toString());
+			    xmlsw.writeEndElement();
+			}
+		    }
 		}
-		bos.close();
+		if (bos != null) {
+		    bos.close();
+		}
 		xmlsw.writeEndElement();
 		xmlsw.flush();
 		output.close();
 		xmlsw.close();
+		if (isExported) {
+		    // connect
+		    URI uri = new URI(targetServerUrl + "/rest/copyfile/copy/");
+		    URL url = uri.toURL();
+		    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-		// connect
-		URI uri = new URI(targetServerUrl + "/rest/copyfile/copy/");
-		URL url = uri.toURL();
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		    // initialize the connection
+		    connection.setDoOutput(true);
+		    connection.setDoInput(true);
+		    connection.setRequestMethod("POST");
+		    connection.setUseCaches(false);
+		    connection.setRequestProperty("Content-type", "text/plain");
+		    connection.setRequestProperty("Connection", "Keep-Alive");
 
-		// initialize the connection
-		connection.setDoOutput(true);
-		connection.setDoInput(true);
-		connection.setRequestMethod("POST");
-		connection.setUseCaches(false);
-		connection.setRequestProperty("Content-type", "text/plain");
-		connection.setRequestProperty("Connection", "Keep-Alive");
+		    OutputStream out = connection.getOutputStream();
+		    BufferedReader reader = new BufferedReader(new FileReader(file.getPath()));
+		    char[] buf = new char[1024];
+		    int numRead = 0;
+		    Date date = new Date();
+		    Timestamp time = new Timestamp(date.getTime());
+		    String[] tab = targetKey.split(":");
+		    String superpassword = tab[1];
+		    String hashCode = SHAMessageDigester.getHash(time.toString() + ":" + superpassword);
+		    StringBuffer param = new StringBuffer();
+		    param.append("timestamp=" + time.toString() + "&&hashcode=" + hashCode + "&&contentsfile=");
+		    while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			param.append(readData);
+		    }
+		    reader.close();
+		    out.write(param.toString().getBytes());
+		    out.flush();
+		    connection.connect();
+		    BufferedReader inStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		    out.close();
+		    String string = null;
+		    while ((string = inStream.readLine()) != null) {
+			log.info("Server: " + string);
+		    }
 
-		OutputStream out = connection.getOutputStream();
-		BufferedReader reader = new BufferedReader(new FileReader(file.getPath()));
-		char[] buf = new char[1024];
-		int numRead = 0;
-		Date date = new Date();
-		Timestamp time = new Timestamp(date.getTime());
-		String[] tab = targetKey.split(":");
-
-		String superpassword = tab[1];
-
-		String hashCode = SHAMessageDigester.getHash(time.toString() + ":" + superpassword);
-		StringBuffer param = new StringBuffer();
-		param.append("timestamp=" + time.toString() + "&&hashcode=" + hashCode + "&&contentsfile=");
-		while ((numRead = reader.read(buf)) != -1) {
-		    String readData = String.valueOf(buf, 0, numRead);
-		    param.append(readData);
+		    connection.disconnect();
 		}
-		reader.close();
-		out.write(param.toString().getBytes());
-		out.flush();
-		connection.connect();
-		BufferedReader inStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		out.close();
-		String string = null;
-		while ((string = inStream.readLine()) != null) {
-		    log.info("Server: " + string);
-		}
-
-		connection.disconnect();
 	    } else {
 		log.info("there is no content to be exported");
 	    }
 
 	    log.info("End Execute ExportContentJob");
 	} catch (Exception ex) {
-	    log.error("error when exporting content");
+	    ex.printStackTrace();
+	    log.error("error when exporting content" + ex.getMessage());
 	} finally {
 	    session.logout();
 	}
 
+    }
+
+    /**
+     * Adds the revision data.
+     * 
+     * @param node
+     *            the node
+     * @param list
+     *            the list
+     * 
+     * @throws Exception
+     *             the exception
+     */
+    private void addRevisionData(Node node, Collection<VersionData> list) throws Exception {
+	List<Value> valueList = new ArrayList<Value>();
+	ValueFactory factory = node.getSession().getValueFactory();
+	for (VersionData versionData : list) {
+	    valueList.add(factory.createValue(versionData.toStringValue()));
+	}
+	node.setProperty(AuthoringPublicationConstant.REVISION_DATA_PROP, valueList.toArray(new Value[] {}));
+    }
+
+    /**
+     * Gets the revision data.
+     * 
+     * @param node
+     *            the node
+     * 
+     * @return the revision data
+     * 
+     * @throws Exception
+     *             the exception
+     */
+    private Map<String, VersionData> getRevisionData(Node node) throws Exception {
+	Map<String, VersionData> map = new HashMap<String, VersionData>();
+	try {
+	    for (Value v : node.getProperty(AuthoringPublicationConstant.REVISION_DATA_PROP).getValues()) {
+		VersionData versionData = VersionData.toVersionData(v.getString());
+		map.put(versionData.getUUID(), versionData);
+	    }
+	} catch (Exception e) {
+	    return map;
+	}
+	return map;
     }
 
 }
