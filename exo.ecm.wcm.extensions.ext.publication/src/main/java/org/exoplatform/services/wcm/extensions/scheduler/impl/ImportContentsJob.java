ffa0a63f6f013545b6f517e980837ff0f7aaa6cc
Dung Van Khuong <dung.khuong@exoplatform.com>
2010-02-03 04:21:04 +0000

    WCM-2540: Import all the related symlinks of the current content

--- exo.ecm.wcm.extensions.ext.publication/src/main/java/org/exoplatform/services/wcm/extensions/scheduler/impl/ImportContentsJob.java
+++ exo.ecm.wcm.extensions.ext.publication/src/main/java/org/exoplatform/services/wcm/extensions/scheduler/impl/ImportContentsJob.java
@@ -6,6 +6,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.Session;
@@ -17,10 +19,12 @@ import org.apache.commons.logging.Log;
 import org.exoplatform.commons.utils.MimeTypeResolver;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.services.cms.link.LinkManager;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.wcm.utils.WCMCoreUtils;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -51,14 +55,11 @@ public class ImportContentsJob implements Job {
 				temporaryStorge = jdatamap.getString("temporaryStorge");
 				log.debug("Init parameters first time :");
 			}
-			SessionProvider sessionProvider = SessionProvider
-					.createSystemProvider();
+			SessionProvider sessionProvider = SessionProvider.createSystemProvider();
 
 			ExoContainer container = ExoContainerContext.getCurrentContainer();
-			RepositoryService repositoryService_ = (RepositoryService) container
-					.getComponentInstanceOfType(RepositoryService.class);
-			ManageableRepository manageableRepository = repositoryService_
-					.getRepository("repository");
+			RepositoryService repositoryService_ = (RepositoryService) container.getComponentInstanceOfType(RepositoryService.class);
+			ManageableRepository manageableRepository = repositoryService_.getRepository("repository");
 			XMLInputFactory factory = XMLInputFactory.newInstance();
 
 			File stagingFolder = new File(stagingStorage);
@@ -69,9 +70,13 @@ public class ImportContentsJob implements Job {
 			XMLStreamReader reader = null;
 			InputStream xmlInputStream = null;
 			int eventType;
+			List<LinkObject> listLink = new ArrayList<LinkObject>();
+      LinkObject linkObj = new LinkObject();
+      boolean hasNewContent =false;
 			if (stagingFolder.exists()) {
 				files = stagingFolder.listFiles();
 				if (files != null) {
+				  hasNewContent = true;
 					for (int i = 0; i < files.length; i++) {
 						xmlFile = files[i];
 						if (xmlFile.isFile()) {
@@ -82,16 +87,12 @@ public class ImportContentsJob implements Job {
 									.getName());
 							if ("text/xml".equals(mimeType)) {
 								xmlInputStream = new FileInputStream(xmlFile);
-								reader = factory
-										.createXMLStreamReader(xmlInputStream);
-
+								reader = factory.createXMLStreamReader(xmlInputStream);												  		          
 								while (reader.hasNext()) {
 									eventType = reader.next();
-									if (eventType == XMLEvent.START_ELEMENT
-											&& "content".equals(reader
-													.getLocalName())) {
-										String content = reader
-												.getElementText();
+									if (eventType == XMLEvent.START_ELEMENT	&& "data".equals(reader.getLocalName())) {
+										String data = reader.getElementText();
+										
 										if (!tempfolder.exists())
 											tempfolder.mkdirs();
 										long time = System.currentTimeMillis();
@@ -100,7 +101,7 @@ public class ImportContentsJob implements Job {
 												+ hashCode + "-" + time
 												+ ".xml.tmp");
 										InputStream inputStream = new ByteArrayInputStream(
-												content.getBytes());
+												data.getBytes());
 										OutputStream out = new FileOutputStream(
 												file);
 										byte[] buf = new byte[1024];
@@ -109,18 +110,34 @@ public class ImportContentsJob implements Job {
 											out.write(buf, 0, len);
 										out.close();
 										inputStream.close();
-
+									}
+									try {
+  									if (eventType == XMLEvent.START_ELEMENT && "content".equals(reader.getLocalName())) {
+  			                linkObj.setSourcePath(reader.getAttributeValue(0)); //--Attribute number 0 = targetPath  									      			           
+  			            }
+  									if (eventType == XMLEvent.START_ELEMENT && "type".equals(reader.getLocalName())) {
+                      linkObj.setLinkType(reader.getElementText());                                           
+  									}
+  									if (eventType == XMLEvent.START_ELEMENT && "title".equals(reader.getLocalName())) {
+                      linkObj.setLinkTitle(reader.getElementText());                                         
+  									}
+  									if (eventType == XMLEvent.START_ELEMENT && "targetPath".equals(reader.getLocalName())) {
+                      linkObj.setLinkTargetPath(reader.getElementText());
+                      listLink.add(linkObj);
+                    }  			
+  									 
+									} catch(Exception ie) {
+									    log.info("Error in ImportContentsJob: "+ie.getMessage());
 									}
 								}
-								reader.close();
-								xmlInputStream.close();
-								xmlFile.delete();
-
+								 reader.close();
+                 xmlInputStream.close();
+                 xmlFile.delete();
 							}
 						}
 					}
 				}
-			}
+			}		
 			files = tempfolder.listFiles();
 			if (files != null) {
 				for (int i = 0; i < files.length; i++) {
@@ -129,8 +146,9 @@ public class ImportContentsJob implements Job {
 					reader = factory.createXMLStreamReader(inputStream);
 					String workspace = null;
 					String nodePath = new String();
-					while (reader.hasNext()) {
-						eventType = reader.next();
+					
+					while (reader.hasNext()) {					  
+						eventType = reader.next();																												
 						if (eventType == XMLEvent.START_ELEMENT) {
 							if (reader.getLocalName().equals("property")) {
 								String value = reader.getAttributeValue(0);
@@ -139,7 +157,6 @@ public class ImportContentsJob implements Job {
 									if (eventType == XMLEvent.START_ELEMENT) {
 										reader.next();
 										nodePath = reader.getText();
-
 									}
 								} else if (MIX_TARGET_WORKSPACE.equals(value)) {
 									eventType = reader.next();
@@ -153,8 +170,7 @@ public class ImportContentsJob implements Job {
 					}
 					reader.close();
 					inputStream.close();
-					Session session = sessionProvider.getSession(workspace,
-							manageableRepository);
+					Session session = sessionProvider.getSession(workspace,	manageableRepository);
 					if (session.itemExists(nodePath))
 						session.getItem(nodePath).remove();
 					session.save();
@@ -174,20 +190,91 @@ public class ImportContentsJob implements Job {
 							}
 							node_ = (Node) session.getItem(path_.toString());
 						}
-
 					}
+					
 					session.importXML(path, new FileInputStream(xmlFile), 0);
 					session.save();
 					xmlFile.delete();
+					
+					if(hasNewContent){					  
+					  for(LinkObject obj : listLink){
+              String[] linkTarget = obj.getLinkTargetPath().split(":");                       
+              String itemPath = "";
+              boolean flag =true;
+              for(int index =2;index< linkTarget.length; index++ ){
+                if(flag){
+                  itemPath += linkTarget[index];                      
+                  flag=false;
+                }else{                      
+                  itemPath += ":";
+                  itemPath += linkTarget[index];
+                }                   
+              }
+              String[] linkSource = obj.getSourcePath().split(":");
+              session = sessionProvider.getSession(linkTarget[1], manageableRepository);
+              Node parentNode = (Node) session.getItem(itemPath);
+            
+              String sourcePath = "";
+              boolean flagSource =true;
+              for(int index =2;index< linkSource.length; index++ ){
+                if(flagSource){
+                  sourcePath += linkSource[index];                      
+                  flagSource=false;
+                }else{                      
+                  sourcePath += ":";
+                  sourcePath += linkSource[index];
+                }                   
+              }               
+                                         
+              if(parentNode.hasNode(obj.getLinkTitle())){
+                Node existedNode = (Node) session.getItem(itemPath+"/"+obj.getLinkTitle());
+                existedNode.remove();
+              } 
+              session = sessionProvider.getSession(linkSource[1], manageableRepository);
+              Node targetNode = (Node) session.getItem(sourcePath);
+              LinkManager linkManager = WCMCoreUtils.getService(LinkManager.class);
+              linkManager.createLink(parentNode, obj.getLinkType(), targetNode, obj.getLinkTitle());              
+            }
+					}
 				}
 			}
 			if (log.isInfoEnabled()) {
 				log.info("End Execute ImportXMLJob");
 			}
 		} catch (Exception ex) {
+		  ex.printStackTrace();
 			log.error("Error when importing Contents");
 		}
-
 	}
 
+	private class LinkObject{
+	  private String linkType;
+	  private String linkTitle;
+	  private String linkTargetPath;
+	  private String sourcePath;
+    public String getLinkType() {
+      return linkType;
+    }
+    public void setLinkType(String linkType) {
+      this.linkType = linkType;
+    }
+    public String getLinkTitle() {
+      return linkTitle;
+    }
+    public void setLinkTitle(String linkTitle) {
+      this.linkTitle = linkTitle;
+    }
+    public String getLinkTargetPath() {
+      return linkTargetPath;
+    }
+    public void setLinkTargetPath(String linkTargetPath) {
+      this.linkTargetPath = linkTargetPath;
+    }
+    public String getSourcePath() {
+      return sourcePath;
+    }
+    public void setSourcePath(String sourcePath) {
+      this.sourcePath = sourcePath;
+    }	  	     
+	}	
 }
