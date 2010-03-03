8869381c7209060386fe39ebf79e73f14c3dda69
Dung Van Khuong <dung.khuong@exoplatform.com>
2010-03-03 04:21:04 +0000

    WCM-2662:  DIV probleme with WCM

--- portlets/presentation/src/main/java/org/exoplatform/wcm/webui/scv/UISingleContentViewerPortlet.java
+++ portlets/presentation/src/main/java/org/exoplatform/wcm/webui/scv/UISingleContentViewerPortlet.java
@@ -41,6 +41,7 @@ import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
  * Jun 9, 2008
  */
 @ComponentConfig(
+		lifecycle = UIApplicationLifecycle.class,
 		template = "app:/groovy/SingleContentViewer/UISingleContentView.gtmpl"    
 )
 
