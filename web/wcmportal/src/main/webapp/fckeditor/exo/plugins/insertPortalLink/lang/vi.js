cfda34dcddb4f2c65bd6b16da1c0bde819b049b6
Dung Van Khuong <dung.khuong@exoplatform.com>
2010-03-29 07:12:49 +0000

    WCM-2717: Problem with Add a site page link on FCKEDITOR

--- web/wcmportal/src/main/webapp/fckeditor/exo/plugins/insertPortalLink/lang/vi.js
+++ web/wcmportal/src/main/webapp/fckeditor/exo/plugins/insertPortalLink/lang/vi.js
@@ -1,7 +1,11 @@
+var UserLanguage= {
+	WCMInsertPortalLinkPlugins : "Chèn liên kết tới một trang của website",	
+	WCMInsertPortalLinkDialogTitle : "Chèn liên kết tới một trang của website",
+	WCMInsertPortalLinkInputTitle : "Tiêu đề: ",
+	WCMInsertPortalLinkInputUrl : "URL: ",
+	WCMInsertPortalLinkButtonGet : "Lấy liên kết website",
+	WCMInsertPortalLinkButtonPreview : "Xem trước",
+	WCMInsertPortalLinkButtonSave : "Lưu"
+}
 FCKLang.WCMInsertPortalLinkPlugins = "Chèn liên kết tới một trang của website";
 FCKLang.WCMInsertPortalLinkDialogTitle = "Chèn liên kết tới một trang của website";
-FCKLang.WCMInsertPortalLinkInputTitle = "Tiêu đề: ";
-FCKLang.WCMInsertPortalLinkInputUrl = "URL: ";
-FCKLang.WCMInsertPortalLinkButtonGet = "Lấy liên kết website";
-FCKLang.WCMInsertPortalLinkButtonPreview = "Xem trước";
-FCKLang.WCMInsertPortalLinkButtonSave = "Lưu";
